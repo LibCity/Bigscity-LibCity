f0b03753c848287d705a8e55e44129099a99e9f7
WenMellors <33283819+WenMellors@users.noreply.github.com>
2021-01-12 11:01:41 +0800

    Trajectory Location Evaluator Rebuild (#18)
    * update evaluate
    
    * fix F1

--- trafficdl/evaluator/traj_loc_pred_evaluator.py
+++ trafficdl/evaluator/traj_loc_pred_evaluator.py
@@ -5,15 +5,21 @@ import time
 
 from trafficdl.evaluator.abstract_evaluator import AbstractEvaluator
 from trafficdl.evaluator.eval_funcs import top_k
-allowed_metrics = ['topk']
+allowed_metrics = ['Precision', 'Recall', 'F1', 'MRR', 'MAP', 'NDCG']
+
 class TrajLocPredEvaluator(AbstractEvaluator):
 
     def __init__(self, config):
         self.metrics = config['metrics'] # 评估指标, 是一个 list
         self.config = config
-        self.topk = 1
+        self.topk = config['topk']
         self.result = {}
-        self.intermediate_result = {} # 分用户存，每个用户下每个指标一个 list 目前不实装分 user 存
+        self.intermediate_result = {
+            'total': 0,
+            'hit': 0,
+            'rank': 0.0,
+            'dcg': 0.0
+        }
         self._check_config()
 
     def _check_config(self):
@@ -22,14 +28,6 @@ class TrajLocPredEvaluator(AbstractEvaluator):
         for i in self.metrics:
             if i not in allowed_metrics:
                 raise ValueError('the metric is not allowed in TrajLocPredEvaluator')
-        if 'topk' in self.metrics:
-            self.topk = self.config['topk']
-        
-        for metric in self.metrics:
-            if metric == 'topk':
-                # 第一元素存放命中的次数，第二个存放总共的次数，每次累加最后就能够计算一个 ac 率了
-                self.intermediate_result[metric] = [0, 0]
-            
     
     def collect(self, batch):
         '''
@@ -41,35 +39,27 @@ class TrajLocPredEvaluator(AbstractEvaluator):
         '''
         if not isinstance(batch, dict):
             raise TypeError('evaluator.collect input is not a dict of user')
-        # 初始化 intermediate_result
-        # TODO: 评估目前就整体返回一个值算了，看不到分 user 统计的必要
-        # for uid in batch:
-        #     if uid not in self.intermediate_result:
-        #         self.intermediate_result[uid] = {}
-        #         for metric in self.metrics:
-        #             self.intermediate_result[uid][metric] = []
-        for metric in self.metrics:
-            if metric == 'topk':
-                res = top_k(batch['loc_pred'], batch['loc_true'], self.topk)
-                unique, counts = np.unique(res, return_counts=True)
-                if counts.shape[0] == 2:
-                    self.intermediate_result[metric][0] += counts[1]
-                    self.intermediate_result[metric][1] += (counts[0] + counts[1])
-                else:
-                    if unique[0] == 0:
-                        self.intermediate_result[metric][1] += counts[0]
-                    else:
-                        self.intermediate_result[metric][0] += counts[0]
-                        self.intermediate_result[metric][1] += counts[0]
-                # # 把评估的结果写到不同的 user 里面
-                # for i, uid in enumerate(batch[uid]):
-                #     self.intermediate_result[metric].append(res[i])
+        hit, rank, dcg = top_k(batch['loc_pred'], batch['loc_true'], self.topk)
+        total = len(batch['loc_true'])
+        self.intermediate_result['total'] += total
+        self.intermediate_result['hit'] += hit
+        self.intermediate_result['rank'] += rank
+        self.intermediate_result['dcg'] += dcg
             
 
     def evaluate(self):
-        for metric in self.metrics:
-            if metric == 'topk':
-                self.result[metric] = self.intermediate_result[metric][0] / self.intermediate_result[metric][1]
+        if 'Precision' in self.metrics or 'F1' in self.metrics:
+            self.result['Precision@{}'.format(self.topk)] = self.intermediate_result['hit'] / (self.intermediate_result['total'] * self.topk)
+        # recall is used to valid in the trainning, so must exit
+        self.result['Recall@{}'.format(self.topk)] = self.intermediate_result['hit'] / self.intermediate_result['total']
+        if 'F1' in self.metrics:
+            self.result['F1@{}'.format(self.topk)] = (2 * self.result['Precision@{}'.format(self.topk)] * self.result['Recall@{}'.format(self.topk)]) / (self.result['Precision@{}'.format(self.topk)] + self.result['Recall@{}'.format(self.topk)])
+        if 'MRR' in self.metrics:
+            self.result['MRR@{}'.format(self.topk)] = self.intermediate_result['rank'] / self.intermediate_result['total']
+        if 'MAP' in self.metrics:
+            self.result['MAP@{}'.format(self.topk)] = self.intermediate_result['rank'] / self.intermediate_result['total']
+        if 'NDCG' in self.metrics:
+            self.result['NDCG@{}'.format(self.topk)] = self.intermediate_result['dcg'] / self.intermediate_result['total']
         return self.result
     
     def save_result(self, save_path, filename=None):
@@ -79,13 +69,15 @@ class TrajLocPredEvaluator(AbstractEvaluator):
         if filename == None:
             # 使用时间戳
             filename = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime(time.time()))
-        print('evaluate result is ', json.dumps(self.result))
+        print('evaluate result is ', json.dumps(self.result, indent=1))
         with open(os.path.join(save_path, '{}.json'.format(filename)), 'w') as f:
             json.dump(self.result, f)
 
     def clear(self):
         self.result = {}
-        for metric in self.metrics:
-            if metric == 'topk':
-                # 第一元素存放命中的次数，第二个存放总共的次数，每次累加最后就能够计算一个 ac 率了
-                self.intermediate_result[metric] = [0, 0]
+        self.intermediate_result = {
+            'total': 0,
+            'hit': 0,
+            'rank': 0.0,
+            'dcg': 0.0
+        }
