2a494310857e2aec9da6e63b7721a6068d333c7d
jjw <jwjiang_buaa@163.com>
2021-01-14 01:40:07 +0800

    add STGCN model

--- trafficdl/model/__init__.py
+++ trafficdl/model/__init__.py
@@ -1,2 +1,2 @@
 from trafficdl.model.trajectory_loc_prediction import DeepMove, RNN
-from trafficdl.model.traffic_speed_prediction import DCRNN
+from trafficdl.model.traffic_speed_prediction import DCRNN, STGCN
