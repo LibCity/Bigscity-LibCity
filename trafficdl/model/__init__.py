10926d32ef241f7bae86f34858c8492383afa4df
Melles <842595644@qq.com>
2021-01-13 20:48:02 +0800

    add import

--- trafficdl/model/__init__.py
+++ trafficdl/model/__init__.py
@@ -1,2 +1,2 @@
-from trafficdl.model.trajectory_loc_prediction import DeepMove, RNN
+from trafficdl.model.trajectory_loc_prediction import DeepMove, RNN, FPMC
 from trafficdl.model.traffic_speed_prediction import DCRNN
