ccc56ea82ca0ae786a83c562e45d6b51a88e532e
jjw <jwjiang_buaa@163.com>
2021-01-18 14:58:17 +0800

    add Graph_WaveNet

---
+++ test/test_gwnet.py
@@ -0,0 +1,57 @@
+from trafficdl.data import get_dataset
+from trafficdl.utils import get_logger, get_executor, get_model
+
+if __name__ == '__main__':
+    config = {
+        'log_level': 'INFO',
+        'input_window': 12,
+        'output_window': 12,
+        'output_dim': 1,
+        'train_rate': 0.7,
+        'eval_rate': 0.1,
+        'cache_dataset': True,
+        'batch_size': 64,
+        'num_workers': 1,
+
+        'evaluator': 'TrafficSpeedPredEvaluator',
+        'dataset_class': 'TrafficSpeedDataset',
+        'executor': 'TrafficSpeedPredExecutor',
+        'model': 'GWNET',
+
+        'learning_rate': 0.001,
+        'learner': 'adam',
+        'weight_decay': 0.0001,
+        'dropout': 0.3,
+        'epochs': 100,
+        'epoch': 0,
+        'max_grad_norm': 5,
+        'clip_grad_norm': True,
+
+        'metrics': ['MAE', 'MSE', 'RMSE', 'MAPE', 'masked_MAE', 'masked_MSE', 'masked_RMSE', 'masked_MAPE', 'R2', 'EVAR'],
+        'gpu': True,
+        'dataset': 'METR_LA',
+        'weight_col': 'cost',
+        'calculate_weight': True,
+        'add_time_in_day': False,
+        'add_day_in_week': False,
+    }
+    logger = get_logger(config)
+    dataset = get_dataset(config)
+    train_data, valid_data, test_data = dataset.get_data()
+    print(len(train_data.dataset), train_data.dataset[0][0].shape, train_data.dataset[0][1].shape,
+          train_data.batch_size)
+    print(len(valid_data.dataset), valid_data.dataset[0][0].shape, valid_data.dataset[0][1].shape,
+          valid_data.batch_size)
+    print(len(test_data.dataset), test_data.dataset[0][0].shape, test_data.dataset[0][1].shape, test_data.batch_size)
+
+    data_feature = dataset.get_data_feature()
+    print(data_feature['adj_mx'].shape)
+    print(data_feature['adj_mx'].sum())
+    model = get_model(config, data_feature)
+    executor = get_executor(config, model)
+    executor.train(train_data, valid_data)
+    model_cache_file = './trafficdl/cache/model_cache/' + config['model'] + '_' + config['dataset'] + '.m'
+    executor.save_model(model_cache_file)
+    executor.load_model(model_cache_file)
+    # 评估，评估结果将会放在 cache/evaluate_cache 下
+    executor.evaluate(test_data)
