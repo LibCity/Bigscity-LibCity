44bfa794d8e41c83a591a1ca5b68b02514b621bb
WenMellors <33283819+WenMellors@users.noreply.github.com>
2021-01-18 19:37:15 +0800

    update (#29)

--- trafficdl/model/abstract_model.py
+++ trafficdl/model/abstract_model.py
@@ -6,12 +6,20 @@ class AbstractModel(nn.Module):
         nn.Module.__init__(self)
         # raise NotImplementedError('Model not implement')
 
-    def forward(self, batch):
+    def predict(self, batch):
         '''
         Args:
             batch (Batch): a batch of input
         Returns:
             scores (tensor, shape = N*C): the scores of the batch, and will use the score to calculate loss or evaluate.
             N is the number of target
+        '''
+    
+    def calculate_loss(self, batch):
+        '''
+        Args:
+            batch (Batch): a batch of input
+        Returns:
+            loss (tensor): return training loss 
         '''
 
