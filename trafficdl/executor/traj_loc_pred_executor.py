c796269ca3071187d6b3fd85e837d36e235d9143
Melles <842595644@qq.com>
2021-01-28 09:51:48 +0800

    testLSTPM

--- trafficdl/executor/traj_loc_pred_executor.py
+++ trafficdl/executor/traj_loc_pred_executor.py
@@ -102,9 +102,9 @@ class TrajLocPredExecutor(AbstractExecutor):
         loss_func = self.loss_func or model.calculate_loss
         for batch in data_loader:
             # one batch, one step
+            optimizer.zero_grad()
             batch.to_tensor(device=self.config['device'])
             loss = loss_func(batch)
-            optimizer.zero_grad()
             loss.backward()
             total_loss.append(loss.data.cpu().numpy().tolist())
             try:
