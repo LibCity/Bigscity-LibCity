ccb1be7544ec18102a1c2bf6fa9fa96785c89879
jjw <jwjiang_buaa@163.com>
2021-01-31 23:39:34 +0800

    agcrn

--- trafficdl/model/traffic_speed_prediction/__init__.py
+++ trafficdl/model/traffic_speed_prediction/__init__.py
@@ -1,3 +1,4 @@
 from trafficdl.model.traffic_speed_prediction.DCRNN import DCRNN
 from trafficdl.model.traffic_speed_prediction.STGCN import STGCN
 from trafficdl.model.traffic_speed_prediction.Graph_WaveNet import GWNET
+from trafficdl.model.traffic_speed_prediction.AGCRN import AGCRN
