0b68f3bf627b501d8d0d0ac87507f8bfaa5ed971
aptx1231 <35984903+aptx1231@users.noreply.github.com>
2021-03-24 04:09:08 -0500

    mstgcn (#73)

--- trafficdl/model/__init__.py
+++ trafficdl/model/__init__.py
@@ -2,11 +2,12 @@ from trafficdl.model.trajectory_loc_prediction import DeepMove, RNN, FPMC, \
     LSTPM, STRNN, TemplateTLP, SERM
 from trafficdl.model.traffic_speed_prediction import DCRNN, STGCN, GWNET, \
     TGCLSTM, TGCN, TemplateTSP
-from trafficdl.model.traffic_flow_prediction import AGCRN, ASTGCN
+from trafficdl.model.traffic_flow_prediction import AGCRN, ASTGCN, MSTGCN
 
 __all__ = [
     "AGCRN",
     "ASTGCN",
+    "MSTGCN",
     "DCRNN",
     "STGCN",
     "GWNET",
