417e9662a4e653f8479faf4935ec11756c846ffe
jjw <jwjiang_buaa@163.com>
2021-03-24 17:06:02 +0800

    mstgcn

--- trafficdl/model/traffic_flow_prediction/__init__.py
+++ trafficdl/model/traffic_flow_prediction/__init__.py
@@ -1,7 +1,9 @@
 from trafficdl.model.traffic_flow_prediction.AGCRN import AGCRN
 from trafficdl.model.traffic_flow_prediction.ASTGCN import ASTGCN
+from trafficdl.model.traffic_flow_prediction.MSTGCN import MSTGCN
 
 __all__ = [
     "AGCRN",
     "ASTGCN",
+    "MSTGCN",
 ]
