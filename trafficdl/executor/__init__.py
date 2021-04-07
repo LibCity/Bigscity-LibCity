5467fcfc891bfa54b30f713f6bdd810427ad1d1e
jjw <jwjiang_buaa@163.com>
2021-04-07 09:21:17 +0800

    mtgnn executor

--- trafficdl/executor/__init__.py
+++ trafficdl/executor/__init__.py
@@ -1,9 +1,11 @@
 from trafficdl.executor.traj_loc_pred_executor import TrajLocPredExecutor
 from trafficdl.executor.traffic_state_executor import TrafficStateExecutor
 from trafficdl.executor.dcrnn_executor import DCRNNExecutor
+from trafficdl.executor.mtgnn_executor import MTGNNExecutor
 
 __all__ = [
     "TrajLocPredExecutor",
     "TrafficStateExecutor",
-    "DCRNNExecutor"
+    "DCRNNExecutor",
+    "MTGNNExecutor"
 ]
---
+++ trafficdl/executor/mtgnn_executor.py
@@ -0,0 +1,130 @@
+import time
+import numpy as np
+import torch
+
+from trafficdl.executor.traffic_state_executor import TrafficStateExecutor
+
+
+class MTGNNExecutor(TrafficStateExecutor):
+    def __init__(self, config, model):
+        TrafficStateExecutor.__init__(self, config, model)
+        self.step_size2 = self.config.get('step_size2', 100)
+        self.num_nodes = self.model.get_data_feature().get('num_nodes')
+        self.num_split = self.config.get('num_split', 1)
+
+    def train(self, train_dataloader, eval_dataloader):
+        """
+        use data to train model with config
+
+        Args:
+            train_dataloader(torch.Dataloader): Dataloader
+            eval_dataloader(torch.Dataloader): Dataloader
+        """
+        self._logger.info('Start training ...')
+        min_val_loss = float('inf')
+        wait = 0
+        best_epoch = 0
+        num_batches = len(train_dataloader)
+        self._logger.info("num_batches:{}".format(num_batches))
+
+        batches_seen = num_batches * self._epoch_num * self.num_split
+        for epoch_idx in range(self._epoch_num, self.epochs):
+            start_time = time.time()
+            losses, batches_seen = self._train_epoch(train_dataloader, epoch_idx, batches_seen)
+            self._writer.add_scalar('training loss', np.mean(losses), batches_seen)
+            self._logger.info("epoch complete!")
+            if self.lr_scheduler is not None:
+                self.lr_scheduler.step()
+            self._logger.info("evaluating now!")
+            val_loss = self._valid_epoch(eval_dataloader, epoch_idx, batches_seen)
+            end_time = time.time()
+
+            if (epoch_idx % self.log_every) == 0:
+                if self.lr_scheduler is not None:
+                    log_lr = self.lr_scheduler.get_last_lr()[0]
+                else:
+                    log_lr = self.learning_rate
+                message = 'Epoch [{}/{}] ({}) train_mae: {:.4f}, val_mae: {:.4f}, lr: {:.6f}, {:.1f}s'. \
+                    format(epoch_idx, self.epochs, batches_seen, np.mean(losses), val_loss,
+                           log_lr, (end_time - start_time))
+                self._logger.info(message)
+
+            if val_loss < min_val_loss:
+                wait = 0
+                if self.saved:
+                    model_file_name = self.save_model_with_epoch(epoch_idx)
+                    self._logger.info('Val loss decrease from {:.4f} to {:.4f}, '
+                                      'saving to {}'.format(min_val_loss, val_loss, model_file_name))
+                min_val_loss = val_loss
+                best_epoch = epoch_idx
+            else:
+                wait += 1
+                if wait == self.patience and self.use_early_stop:
+                    self._logger.warning('Early stopping at epoch: %d' % epoch_idx)
+                    break
+        self.load_model_with_epoch(best_epoch)
+
+    def _train_epoch(self, train_dataloader, epoch_idx, batches_seen=None, loss_func=None):
+        """
+        完成模型一个轮次的训练
+
+        Args:
+            train_dataloader: 训练数据
+            epoch_idx: 轮次数
+            batches_seen: 全局batch数
+            loss_func: 损失函数
+
+        Returns:
+            tuple: tuple contains
+                losses(list): 每个batch的损失的数组 \n
+                batches_seen(int): 全局batch数
+        """
+        self.model.train()
+        loss_func = loss_func if loss_func is not None else self.model.calculate_loss
+        losses = []
+        for iter_, batch in enumerate(train_dataloader):
+            self.optimizer.zero_grad()
+            batch.to_tensor(self.device)
+            if iter_ % self.step_size2 == 0:
+                perm = np.random.permutation(range(self.num_nodes))
+            num_sub = int(self.num_nodes / self.num_split)
+            for j in range(self.num_split):
+                if j != self.num_split - 1:
+                    idx = perm[j * num_sub:(j + 1) * num_sub]
+                else:
+                    idx = perm[j * num_sub:]
+                loss = loss_func(batch, idx=idx, batches_seen=batches_seen)
+                self._logger.debug(loss.item())
+                losses.append(loss.item())
+                batches_seen += 1
+                loss.backward()
+                if self.clip_grad_norm:
+                    torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
+                self.optimizer.step()
+        return losses, batches_seen
+
+    def _valid_epoch(self, eval_dataloader, epoch_idx, batches_seen=None, loss_func=None):
+        """
+        完成模型一个轮次的评估
+
+        Args:
+            eval_dataloader: 评估数据
+            epoch_idx: 轮次数
+            batches_seen: 全局batch数
+            loss_func: 损失函数
+
+        Returns:
+            float: 评估数据的平均损失值
+        """
+        with torch.no_grad():
+            self.model.eval()
+            loss_func = loss_func if loss_func is not None else self.model.calculate_loss
+            losses = []
+            for batch in eval_dataloader:
+                batch.to_tensor(self.device)
+                loss = loss_func(batch)
+                self._logger.debug(loss.item())
+                losses.append(loss.item())
+            mean_loss = np.mean(losses)
+            self._writer.add_scalar('eval loss', mean_loss, batches_seen)
+            return mean_loss
