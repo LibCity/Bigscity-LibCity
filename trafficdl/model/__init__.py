da858d79b8dc18d6040a275ab49fb3fa9ab838d4
jjw <jwjiang_buaa@163.com>
2021-04-18 00:10:27 +0800

    stresnet

--- trafficdl/model/__init__.py
+++ trafficdl/model/__init__.py
@@ -2,13 +2,15 @@ from trafficdl.model.trajectory_loc_prediction import DeepMove, RNN, FPMC, \
     LSTPM, STRNN, TemplateTLP, SERM
 from trafficdl.model.traffic_speed_prediction import DCRNN, STGCN, GWNET, \
     MTGNN, TGCLSTM, TGCN, TemplateTSP
-from trafficdl.model.traffic_flow_prediction import AGCRN, ASTGCN, MSTGCN, ACFM
+from trafficdl.model.traffic_flow_prediction import AGCRN, ASTGCN, MSTGCN, \
+    ACFM, STResNet
 
 __all__ = [
     "AGCRN",
     "ASTGCN",
     "MSTGCN",
     "ACFM",
+    "STResNet",
     "DCRNN",
     "STGCN",
     "MTGNN",
--- trafficdl/model/traffic_flow_prediction/__init__.py
+++ trafficdl/model/traffic_flow_prediction/__init__.py
@@ -2,10 +2,12 @@ from trafficdl.model.traffic_flow_prediction.AGCRN import AGCRN
 from trafficdl.model.traffic_flow_prediction.ASTGCN import ASTGCN
 from trafficdl.model.traffic_flow_prediction.MSTGCN import MSTGCN
 from trafficdl.model.traffic_flow_prediction.ACFM import ACFM
+from trafficdl.model.traffic_flow_prediction.STResNet import STResNet
 
 __all__ = [
     "AGCRN",
     "ASTGCN",
     "MSTGCN",
-    "ACFM"
+    "ACFM",
+    "STResNet"
 ]
