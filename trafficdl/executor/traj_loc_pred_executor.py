4e51825b5f5233417ce4abd370b13d6b52b35201
WenMellors <33283819+WenMellors@users.noreply.github.com>
2021-04-17 22:16:24 +0800

    Jwj test serm (#86)
    * refactor traj executor
    
    * update serm config
    
    * fix serm loss func
    
    * add optimizer to serm config
    
    * executor add eval loss
    
    * fix serm bug
    
    * fix tow softmax bug
    
    * update SERM config

--- trafficdl/executor/traj_loc_pred_executor.py
+++ trafficdl/executor/traj_loc_pred_executor.py
@@ -3,6 +3,7 @@ import torch
 import torch.optim as optim
 import numpy as np
 import os
+from logging import getLogger
 
 from trafficdl.executor.abstract_executor import AbstractExecutor
 from trafficdl.utils import get_evaluator
@@ -19,12 +20,9 @@ class TrajLocPredExecutor(AbstractExecutor):
         self.cache_dir = './trafficdl/cache/model_cache'
         self.evaluate_res_dir = './trafficdl/cache/evaluate_cache'
         self.loss_func = None  # TODO: 根据配置文件支持选择特定的 Loss Func 目前并未实装
-        self.optimizer = optim.Adam(filter(lambda p: p.requires_grad, self.model.parameters()),
-                                    lr=self.config['learning_rate'], weight_decay=self.config['L2'])
-        self.scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'max',
-                                                              patience=self.config['lr_step'],
-                                                              factor=self.config['lr_decay'],
-                                                              threshold=self.config['schedule_threshold'])
+        self._logger = getLogger()
+        self.optimizer = self._build_optimizer()
+        self.scheduler = self._build_scheduler()
 
     def train(self, train_dataloader, eval_dataloader):
         if not os.path.exists(self.tmp_path):
@@ -42,12 +40,12 @@ class TrajLocPredExecutor(AbstractExecutor):
                 train_dataloader, self.model,
                 self.config['learning_rate'], self.config['clip'],
                 train_total_batch, self.config['verbose'])
-            print('==>Train Epoch:{:0>2d} Loss:{:.4f} learning_rate:{}'.format(
+            self._logger.info('==>Train Epoch:{:4d} Loss:{:.5f} learning_rate:{}'.format(
                 epoch, avg_loss, lr))
             # eval stage
             avg_eval_acc, avg_eval_loss = self._valid_epoch(eval_dataloader, self.model,
                                                             eval_total_batch, self.config['verbose'])
-            print('==>Eval Acc:{:.4f}'.format(avg_eval_acc))
+            self._logger.info('==>Eval Acc:{:.5f} Eval Loss:{:.5f}'.format(avg_eval_acc, avg_eval_loss))
             metrics['accuracy'].append(avg_eval_acc)
             metrics['loss'].append(avg_eval_loss)
             if self.config['hyper_tune']:
@@ -60,8 +58,12 @@ class TrajLocPredExecutor(AbstractExecutor):
             else:
                 save_name_tmp = 'ep_' + str(epoch) + '.m'
                 torch.save(self.model.state_dict(), self.tmp_path + save_name_tmp)
-            # 这个 scheduler 看起来也没什么用呀？ TODO: 发现一下他的作用
             self.scheduler.step(avg_eval_acc)
+            # scheduler 会根据 avg_eval_acc 减小学习率
+            # 若当前学习率小于特定值，则 early stop
+            lr = self.optimizer.param_groups[0]['lr']
+            if lr < self.config['early_stop_lr']:
+                break
         if not self.config['hyper_tune'] and self.config['load_best_epoch']:
             best = np.argmax(metrics['accuracy'])  # 这个不是最好的一次吗？
             load_name_tmp = 'ep_' + str(best) + '.m'
@@ -101,7 +103,7 @@ class TrajLocPredExecutor(AbstractExecutor):
             }
             cnt += 1
             if cnt % self.config['verbose'] == 0:
-                print('finish batch {}/{}'.format(cnt, test_total_batch))
+                self._logger.info('finish batch {}/{}'.format(cnt, test_total_batch))
             self.evaluator.collect(evaluate_input)
         self.evaluator.save_result(self.evaluate_res_dir)
 
@@ -128,7 +130,7 @@ class TrajLocPredExecutor(AbstractExecutor):
             self.optimizer.step()
             cnt += 1
             if cnt % verbose == 0:
-                print('finish batch {}/{}'.format(cnt, total_batch))
+                self._logger.info('finish batch {}/{}'.format(cnt, total_batch))
         avg_loss = np.mean(total_loss, dtype=np.float64)
         return model, avg_loss
 
@@ -150,8 +152,42 @@ class TrajLocPredExecutor(AbstractExecutor):
             }
             cnt += 1
             if cnt % verbose == 0:
-                print('finish batch {}/{}'.format(cnt, total_batch))
+                self._logger.info('finish batch {}/{}'.format(cnt, total_batch))
             self.evaluator.collect(evaluate_input)
         avg_acc = self.evaluator.evaluate()[self.metrics]  # 随便选一个就行
         avg_loss = np.mean(total_loss, dtype=np.float64)
         return avg_acc, avg_loss
+
+    def _build_optimizer(self):
+        """
+        根据全局参数`learner`选择optimizer
+        """
+        if self.config['optimizer'] == 'adam':
+            optimizer = optim.Adam(self.model.parameters(), lr=self.config['learning_rate'],
+                                   weight_decay=self.config['L2'])
+        elif self.config['optimizer'] == 'sgd':
+            optimizer = torch.optim.SGD(self.model.parameters(), lr=self.config['learning_rate'],
+                                        weight_decay=self.config['L2'])
+        elif self.config['optimizer'] == 'adagrad':
+            optimizer = torch.optim.Adagrad(self.model.parameters(), lr=self.config['learning_rate'],
+                                            weight_decay=self.config['L2'])
+        elif self.config['optimizer'] == 'rmsprop':
+            optimizer = torch.optim.RMSprop(self.model.parameters(), lr=self.config['learning_rate'],
+                                            weight_decay=self.config['L2'])
+        elif self.config['optimizer'] == 'sparse_adam':
+            optimizer = torch.optim.SparseAdam(self.model.parameters(), lr=self.config['learning_rate'])
+        else:
+            self._logger.warning('Received unrecognized optimizer, set default Adam optimizer')
+            optimizer = optim.Adam(self.model.parameters(), lr=self.config['learning_rate'],
+                                   weight_decay=self.config['L2'])
+        return optimizer
+
+    def _build_scheduler(self):
+        """
+        目前就固定的 scheduler 吧
+        """
+        scheduler = optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, 'max',
+                                                         patience=self.config['lr_step'],
+                                                         factor=self.config['lr_decay'],
+                                                         threshold=self.config['schedule_threshold'])
+        return scheduler
--- trafficdl/model/trajectory_loc_prediction/SERM.py
+++ trafficdl/model/trajectory_loc_prediction/SERM.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 from trafficdl.model.abstract_model import AbstractModel
+from torch.nn.utils.rnn import pack_padded_sequence
+from torch.nn.utils.rnn import pad_packed_sequence
 
 
 class EmbeddingMatrix(nn.Module):  # text_embdeding
@@ -36,7 +38,7 @@ class SERM(AbstractModel):
         self.user_size = data_feature['uid_size']
         self.user_emb_size = data_feature['loc_size']  # 根据论文
         self.text_size = data_feature['text_size']
-        self.text_emb_size = config['text_emb_size']
+        self.text_emb_size = len(data_feature['word_vec'][0])  # 这个受限于 word_vec 的长度
         self.hidden_size = config['hidden_size']
 
         # Embedding layer
@@ -50,75 +52,67 @@ class SERM(AbstractModel):
         # lstm layer
         self.lstm = nn.LSTM(input_size=self.loc_emb_size + self.tim_emb_size + self.text_emb_size,
                             hidden_size=self.hidden_size)
-
+        # self.lstm = nn.LSTM(input_size=self.loc_emb_size + self.tim_emb_size, hidden_size=self.hidden_size)
         # dense layer
         self.dense = nn.Linear(in_features=self.hidden_size, out_features=self.loc_size)
+        # init weight
+        self.apply(self._init_weight)
+
+    def _init_weight(self, module):
+        if isinstance(module, nn.Embedding):
+            nn.init.xavier_normal_(module.weight)
+        elif isinstance(module, nn.Linear):
+            nn.init.xavier_uniform_(module.weight)
+        elif isinstance(module, nn.LSTM):
+            for name, param in module.named_parameters():
+                if 'weight_ih' in name:
+                    nn.init.xavier_uniform_(param.data)
+                elif 'weight_hh' in name:
+                    nn.init.orthogonal_(param.data)
+                elif 'bias' in name:
+                    nn.init.constant_(param.data, 0)
 
     def forward(self, batch):
-        # input
-        # batch*seq
-        # user batch
+
         loc = batch['current_loc']
         tim = batch['current_tim']
         user = batch['uid']
         text = batch['text']
 
-        # print(self.loc_size)
-        # print(self.loc_emb_size)
-        # print(self.tim_size)
-        # print(self.tim_emb_size)
-        # print(self.user_size)
-        # print(self.user_emb_size)
-        # print(self.text_size)
-        # print(self.text_emb_size)
-        # print(self.hidden_size)
-
-        # print(loc.size())
-        # print(tim.size())
-        # print(user.size())
-        # print(text.size())
-
-        # batch*seq*emb_size
-        # user batch*emb_size
         loc_emb = self.emb_loc(loc)
         tim_emb = self.emb_tim(tim)
         user_emb = self.emb_user(user)
         text_emb = self.emb_text(text)
 
-        # print(loc_emb.size())
-        # print(tim_emb.size())
-        # print(user_emb.size())
-        # print(text_emb.size())
         # change batch*seq*emb_size to seq*batch*emb_size
-        attrs_latent = torch.cat([loc_emb, tim_emb, text_emb], dim=2).permute(1, 0, 2)
+        x = torch.cat([loc_emb, tim_emb, text_emb], dim=2).permute(1, 0, 2)
+        # attrs_latent = torch.cat([loc_emb, tim_emb], dim=2).permute(1, 0, 2)
         # print(attrs_latent.size())
-
-        lstm_out, (h_n, c_n) = self.lstm(attrs_latent)  # seq*batch*hidden_size
+        # pack attrs_latent
+        seq_len = batch.get_origin_len('current_loc')
+        pack_x = pack_padded_sequence(x, lengths=seq_len, enforce_sorted=False)
+        lstm_out, (h_n, c_n) = self.lstm(pack_x)  # seq*batch*hidden_size
         # print(lstm_out.size())
+        # unpack
+        lstm_out, out_len = pad_packed_sequence(lstm_out, batch_first=True)
+        # user_emb is batch*loc_size, so we need get the final lstm_out
+        for i in range(lstm_out.shape[0]):
+            if i == 0:
+                out = lstm_out[0][seq_len[i] - 1].reshape(1, -1)  # .reshape(1,-1)表示：转化为1行
+            else:
+                out = torch.cat((out, lstm_out[i][seq_len[i] - 1].reshape(1, -1)), 0)
+        dense = self.dense(out)  # batch * loc_size
 
-        dense = self.dense(lstm_out)  # seq*batch*loc_size
-        # print(dense.size())
-        # get seq*batch*loc_size then change to batch*seq*loc_size
-        out_vec = torch.add(dense, user_emb).permute(1, 0, 2)
-        # print(out_vec.size())
-
-        pred = nn.Softmax(dim=2)(out_vec)  # result batch*seq*loc_size
+        out_vec = torch.add(dense, user_emb)  # batch * loc_size
+        pred = nn.LogSoftmax(dim=1)(out_vec)  # result
         # print(pred.size())
 
-        # 由于预测的是下一跳，所以选择有效轨迹（补齐前）的最后一个位置的预测值作为实际输出值
-        loc_len = batch.get_origin_len('current_loc')
-        for i in range(pred.shape[0]):
-            if i == 0:
-                true_pred = pred[0][loc_len[i] - 1].reshape(1, -1)  # .reshape(1,-1)表示：转化为1行
-            else:
-                true_pred = torch.cat((true_pred, pred[i][loc_len[i] - 1].reshape(1, -1)), 0)
-
-        return true_pred  # batch*loc_size
+        return pred  # batch*loc_size
 
     def predict(self, batch):
         return self.forward(batch)
 
     def calculate_loss(self, batch):
-        criterion = nn.NLLLoss()  # CrossEntropyLoss: 1)log softmax 2)cross entropy
+        criterion = nn.NLLLoss()
         scores = self.forward(batch)  # batch*loc_size
         return criterion(scores, batch['target'])
