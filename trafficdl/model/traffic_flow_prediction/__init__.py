ad481313b94fd7180dd8613be1e5766ecd36bf6b
jjw <jwjiang_buaa@163.com>
2021-04-17 15:53:05 +0800

    add ACFM

--- trafficdl/model/traffic_flow_prediction/__init__.py
+++ trafficdl/model/traffic_flow_prediction/__init__.py
@@ -1,9 +1,11 @@
 from trafficdl.model.traffic_flow_prediction.AGCRN import AGCRN
 from trafficdl.model.traffic_flow_prediction.ASTGCN import ASTGCN
 from trafficdl.model.traffic_flow_prediction.MSTGCN import MSTGCN
+from trafficdl.model.traffic_flow_prediction.ACFM import ACFM
 
 __all__ = [
     "AGCRN",
     "ASTGCN",
     "MSTGCN",
+    "ACFM"
 ]
