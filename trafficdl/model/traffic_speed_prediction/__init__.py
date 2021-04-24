d73f4db33ecb61c55a76a3d03cc66026836b2fbf
jjw <jwjiang_buaa@163.com>
2021-04-24 21:28:13 +0800

    RNN

--- trafficdl/model/traffic_speed_prediction/__init__.py
+++ trafficdl/model/traffic_speed_prediction/__init__.py
@@ -4,6 +4,7 @@ from trafficdl.model.traffic_speed_prediction.GWNET import GWNET
 from trafficdl.model.traffic_speed_prediction.MTGNN import MTGNN
 from trafficdl.model.traffic_speed_prediction.TGCLSTM import TGCLSTM
 from trafficdl.model.traffic_speed_prediction.TGCN import TGCN
+from trafficdl.model.traffic_speed_prediction.RNN import RNN
 from trafficdl.model.traffic_speed_prediction.TemplateTSP import TemplateTSP
 
 __all__ = [
@@ -13,5 +14,6 @@ __all__ = [
     "TGCLSTM",
     "TGCN",
     "TemplateTSP",
+    "RNN",
     "MTGNN",
 ]
