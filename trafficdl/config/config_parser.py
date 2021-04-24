f98584a164047113747a596a2af6a36132e16ec4
jjw <jwjiang_buaa@163.com>
2021-04-24 21:27:11 +0800

    config

--- trafficdl/config/config_parser.py
+++ trafficdl/config/config_parser.py
@@ -87,8 +87,8 @@ class ConfigParser(object):
                 self.config['evaluator'] = task_config[model]['evaluator']
             # 对于 LSTM RNN GRU 使用的都是同一个类，只是 RNN 模块不一样而已，这里做一下修改
             if self.config['model'].upper() in ['LSTM', 'GRU', 'RNN']:
-                self.config['model'] = 'RNN'
                 self.config['rnn_type'] = self.config['model']
+                self.config['model'] = 'RNN'
             if self.config['dataset'] not in task_config['allowed_dataset']:
                 raise ValueError('task {} do not support dataset {}'.format(
                     self.config['task'], self.config['dataset']))
