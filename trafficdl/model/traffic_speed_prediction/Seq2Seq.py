40835f831c6ca7ad5e04887905bcf536caf41b38
jjw <jwjiang_buaa@163.com>
2021-04-25 20:56:58 +0800

    LSTM is diff

--- trafficdl/model/traffic_speed_prediction/Seq2Seq.py
+++ trafficdl/model/traffic_speed_prediction/Seq2Seq.py
@@ -35,10 +35,17 @@ class Encoder(nn.Module):
         # x = [seq_len, batch_size, input_size]
         # h_0 = [layers * num_directions, batch_size, hidden_size]
         h_0 = torch.zeros(self.layers * self.num_directions, x.shape[1], self.hidden_size).to(self.device)
-        out, hidden = self.rnn(x, h_0)
-        # output = [seq_len, batch_size, hidden_size * num_directions]
-        # hidden = [layers * num_directions, batch_size, hidden_size]
-        return hidden
+        if self.rnn_type == 'LSTM':
+            c_0 = torch.zeros(self.layers * self.num_directions, x.shape[1], self.hidden_size).to(self.device)
+            out, (hn, cn) = self.rnn(x, (h_0, c_0))
+            # output = [seq_len, batch_size, hidden_size * num_directions]
+            # hn/cn = [layers * num_directions, batch_size, hidden_size]
+        else:
+            out, hn = self.rnn(x, h_0)
+            cn = torch.zeros(hn.shape)
+            # output = [seq_len, batch_size, hidden_size * num_directions]
+            # hn = [layers * num_directions, batch_size, hidden_size]
+        return hn, cn
 
 
 class Decoder(nn.Module):
@@ -67,17 +74,21 @@ class Decoder(nn.Module):
             raise ValueError('Unknown RNN type: {}'.format(self.rnn_type))
         self.fc = nn.Linear(hidden_size * self.num_directions, input_size)
 
-    def forward(self, x, hidden):
+    def forward(self, x, hn, cn):
         # x = [batch_size, input_size]
-        # hidden = [layers * num_directions, batch_size, hidden_size]
+        # hn, cn = [layers * num_directions, batch_size, hidden_size]
         x = x.unsqueeze(0)
         # x = [seq_len = 1, batch_size, input_size]
-        out, hidden = self.rnn(x, hidden)
+        if self.rnn_type == 'LSTM':
+            out, (hn, cn) = self.rnn(x, (hn, cn))
+        else:
+            out, hn = self.rnn(x, hn)
+            cn = torch.zeros(hn.shape)
         # out = [seq_len = 1, batch_size, hidden_size * num_directions]
-        # hidden = [layers * num_directions, batch_size, hidden_size]
+        # hn = [layers * num_directions, batch_size, hidden_size]
         out = self.fc(out.squeeze(0))
         # out = [batch_size, input_size]
-        return out, hidden
+        return out, hn, cn
 
 
 class Seq2Seq(AbstractTrafficStateModel):
@@ -104,6 +115,7 @@ class Seq2Seq(AbstractTrafficStateModel):
                                self.hidden_size, self.num_layers, self.dropout, self.bidirectional)
         self.decoder = Decoder(self.device, self.rnn_type, self.num_nodes * self.output_dim,
                                self.hidden_size, self.num_layers, self.dropout, self.bidirectional)
+        self._logger.info('You select rnn_type {} in Seq2Seq!'.format(self.rnn_type))
 
     def forward(self, batch):
         src = batch['X']  # [batch_size, input_window, num_nodes, feature_dim]
@@ -118,17 +130,19 @@ class Seq2Seq(AbstractTrafficStateModel):
         # src = [self.input_window, batch_size, self.num_nodes * self.feature_dim]
         # target = [self.output_window, batch_size, self.num_nodes * self.output_dim]
 
-        encoder_hidden_state = self.encoder(src)
-        decoder_hidden_state = encoder_hidden_state
+        encoder_hn, encoder_cn = self.encoder(src)
+        decoder_hn = encoder_hn
+        decoder_cn = encoder_cn
         # encoder_hidden_state = [layers * num_directions, batch_size, hidden_size]
         decoder_input = torch.randn(batch_size, self.num_nodes * self.output_dim).to(self.device)
         # decoder_input = [batch_size, self.num_nodes * self.output_dim]
 
         outputs = []
         for i in range(self.output_window):
-            decoder_output, decoder_hidden_state = self.decoder(decoder_input, decoder_hidden_state)
+            decoder_output, decoder_hn, decoder_cn = \
+                self.decoder(decoder_input, decoder_hn, decoder_cn)
             # decoder_output = [batch_size, self.num_nodes * self.output_dim]
-            # decoder_hidden_state = [layers * num_directions, batch_size, hidden_size]
+            # decoder_hn = [layers * num_directions, batch_size, hidden_size]
             outputs.append(decoder_output.reshape(batch_size, self.num_nodes, self.output_dim))
             # 只有训练的时候才考虑用真值
             if self.training and random.random() < self.teacher_forcing_ratio:
