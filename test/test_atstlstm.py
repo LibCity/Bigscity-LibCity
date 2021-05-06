25742bd48d49ab2c474b07abb03fbc097e2892a1
Melles <842595644@qq.com>
2021-05-06 20:41:08 +0800

    update test script

--- test/test_atstlstm.py
+++ test/test_atstlstm.py
@@ -1,8 +1,24 @@
 from trafficdl.config import ConfigParser
 from trafficdl.data import get_dataset
-
+from trafficdl.utils import get_model, get_evaluator
 # load config
-config = ConfigParser('traj_loc_pred', 'ATSTLSTM', 'foursquare_tky', None, None)
+config = ConfigParser('traj_loc_pred', 'ATSTLSTM', 'foursquare_tky', other_args={'min_sessions': 5})
 
 dataset = get_dataset(config)
 train_data, valid_data, test_data = dataset.get_data()
+data_feature = dataset.get_data_feature()
+
+model = get_model(config, data_feature).to(config['device'])
+batch = train_data.__iter__().__next__()
+batch.to_tensor(config['device'])
+
+score = model.predict(batch)
+loc_true = [0] * config['batch_size']
+evaluate_input = {
+    'uid': batch['uid'].tolist(),
+    'loc_true': loc_true,
+    'loc_pred': score.tolist()
+}
+
+evaluator = get_evaluator(config)
+evaluator.collect(evaluate_input)
