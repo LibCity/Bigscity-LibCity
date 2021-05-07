7a0b686ffc226e8a97567bb0f81560d1074384f9
XBR-1111 <18231111@buaa.edu.cn>
2021-05-07 21:50:40 +0800

    reset run_model.py and test_model.py

--- test_model.py
+++ test_model.py
@@ -3,15 +3,14 @@ from trafficdl.data import get_dataset
 from trafficdl.utils import get_model, get_executor
 
 # 加载配置文件
-# config = ConfigParser(task='traj_loc_pred', model='TemplateTLP',
-#                       dataset='foursquare_tky', config_file=None,
-#                       other_args={'batch_size': 2})
+config = ConfigParser(task='traj_loc_pred', model='TemplateTLP',
+                      dataset='foursquare_tky', config_file=None,
+                      other_args={'batch_size': 2})
 # 如果是交通流量\速度预测任务，请使用下面的加载配置文件语句
-config = ConfigParser(task='traffic_state_pred', model='ToGCN',
-                      dataset='METR_LA', config_file=None, other_args={'batch_size': 2})
+# config = ConfigParser(task='traffic_state_pred', model='TemplateTSP',
+#       dataset='METR_LA', config_file=None, other_args={'batch_size': 2})
 # 加载数据模块
 dataset = get_dataset(config)
-# print(type(dataset))
 # 数据预处理，划分数据集
 train_data, valid_data, test_data = dataset.get_data()
 data_feature = dataset.get_data_feature()
