9bf9bc5760aab1e8c85c723b62bc428334f67f71
XBR-1111 <59074275+XBR-1111@users.noreply.github.com>
2021-05-07 22:38:24 +0800

    ToGCN (#104)
    * add ToGCN
    
    * modify ToGCN to fit multi-dimensional input
    
    * feature_dim and output_dim of ToGCN
    
    * reset run_model.py and test_model.py
    
    * uncomment _scaler
    
    * uncomment _scaler
    
    * uncomment _scaler
    
    * delete detach and to device
    
    * delete 4 to device

--- trafficdl/model/traffic_flow_prediction/__init__.py
+++ trafficdl/model/traffic_flow_prediction/__init__.py
@@ -6,7 +6,7 @@ from trafficdl.model.traffic_flow_prediction.STResNet import STResNet
 from trafficdl.model.traffic_flow_prediction.STResNetCommon import STResNetCommon
 from trafficdl.model.traffic_flow_prediction.ACFMCommon import ACFMCommon
 from trafficdl.model.traffic_flow_prediction.ASTGCNCommon import ASTGCNCommon
-
+from trafficdl.model.traffic_flow_prediction.ToGCN import ToGCN
 __all__ = [
     "AGCRN",
     "ASTGCN",
@@ -15,5 +15,6 @@ __all__ = [
     "STResNet",
     "STResNetCommon",
     "ACFMCommon",
-    "ASTGCNCommon"
+    "ASTGCNCommon",
+    "ToGCN",
 ]
