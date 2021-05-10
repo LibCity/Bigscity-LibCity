26f510be9f60f0a55220748cc615313a108d2582
aptx1231 <35984903+aptx1231@users.noreply.github.com>
2021-05-10 22:49:02 +0800

    share (#107)

--- trafficdl/model/traffic_demand_prediction/__init__.py
+++ trafficdl/model/traffic_demand_prediction/__init__.py
@@ -1,5 +1,7 @@
 from trafficdl.model.traffic_demand_prediction.STG2Seq import STG2Seq
+from trafficdl.model.traffic_demand_prediction.SHARE import SHARE
 
 __all__ = [
-    "STG2Seq"
+    "STG2Seq",
+    "SHARE"
 ]
