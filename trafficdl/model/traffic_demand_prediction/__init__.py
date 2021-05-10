70364c8d2541843b810974fb94b122f28c6f6a30
jjw <jwjiang_buaa@163.com>
2021-05-10 22:52:26 +0800

    dmvstnet

--- trafficdl/model/traffic_demand_prediction/__init__.py
+++ trafficdl/model/traffic_demand_prediction/__init__.py
@@ -1,7 +1,9 @@
 from trafficdl.model.traffic_demand_prediction.STG2Seq import STG2Seq
 from trafficdl.model.traffic_demand_prediction.SHARE import SHARE
+from trafficdl.model.traffic_demand_prediction.DMVSTNet import DMVSTNet
 
 __all__ = [
     "STG2Seq",
-    "SHARE"
+    "SHARE",
+    "DMVSTNet"
 ]
