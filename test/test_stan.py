d7eb9cc770382146c09842d1a2c6d3620b6c5068
Melles <842595644@qq.com>
2021-06-07 15:40:59 +0800

    add STAN push to remote to test

---
+++ test/test_stan.py
@@ -0,0 +1,16 @@
+# import os
+from trafficdl.config import ConfigParser
+from trafficdl.data import get_dataset
+# from trafficdl.utils import get_executor, get_model
+# import pandas as pd
+
+
+config = ConfigParser('traj_loc_pred', 'STAN', 'foursquare_nyc', other_args={'min_checkins': 5, 'min_sessions': 3,
+                                                                             'min_session_len': 5,
+                                                                             'cut_method': 'same_date',
+                                                                             'max_session_len': 50})
+dataset = get_dataset(config)
+train_data, valid_data, test_data = dataset.get_data()
+# data_feature = dataset.get_data_feature()
+# batch = valid_data.__iter__().__next__()
+# batch.to_tensor(config['device'])
--- trafficdl/data/dataset/trajectory_encoder/__init__.py
+++ trafficdl/data/dataset/trajectory_encoder/__init__.py
@@ -2,10 +2,12 @@ from .standard_trajectory_encoder import StandardTrajectoryEncoder
 from .lstpm_encoder import LstpmEncoder
 from .atstlstm_encoder import AtstlstmEncoder
 from .serm_encoder import SermEncoder
+from .stan_encoder import StanEncoder
 
 __all__ = [
     "StandardTrajectoryEncoder",
     "LstpmEncoder",
     "AtstlstmEncoder",
-    "SermEncoder"
+    "SermEncoder",
+    "StanEncoder"
 ]
