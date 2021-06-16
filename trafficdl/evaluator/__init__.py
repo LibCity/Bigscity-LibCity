c5872f098b4879104593e85da664b53e417ef7c3
WenMellors <33283819+WenMellors@users.noreply.github.com>
2021-06-16 21:56:41 +0800

    add cara (#128)

--- trafficdl/evaluator/__init__.py
+++ trafficdl/evaluator/__init__.py
@@ -1,7 +1,9 @@
 from trafficdl.evaluator.traj_loc_pred_evaluator import TrajLocPredEvaluator
 from trafficdl.evaluator.traffic_state_evaluator import TrafficStateEvaluator
+from trafficdl.evaluator.cara_loc_pred_evaluator import CARALocPredEvaluator
 
 __all__ = [
     "TrajLocPredEvaluator",
-    "TrafficStateEvaluator"
+    "TrafficStateEvaluator",
+    "CARALocPredEvaluator"
 ]
