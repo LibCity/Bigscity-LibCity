030ba251dfb99d4cecc8a145a4b9b22f17424331
NickHan-cs <1152090685@qq.com>
2021-07-12 19:37:30 +0800

    modify metrics

--- libtraffic/model/traffic_speed_prediction/STTN.py
+++ libtraffic/model/traffic_speed_prediction/STTN.py
@@ -326,7 +326,7 @@ class STTN(AbstractTrafficStateModel):
         y_predicted = self.predict(batch)
         y_true = self._scaler.inverse_transform(y_true[..., :self.output_dim])
         y_predicted = self._scaler.inverse_transform(y_predicted[..., :self.output_dim])
-        return loss.masked_mae_torch(y_predicted, y_true, 0)
+        return loss.masked_mae_torch(y_predicted, y_true)
     
     def predict(self, batch):
         return self.forward(batch)
