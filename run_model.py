3e5081e4651f005933fd3e4117a8132cb4959921
XBR-1111 <18231111@buaa.edu.cn>
2021-08-27 19:08:05 +0800

    add Map Matching task

--- run_model.py
+++ run_model.py
@@ -24,11 +24,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     # 增加指定的参数
     parser.add_argument('--task', type=str,
-                        default='map_matching', help='the name of task')
+                        default='traffic_state_pred', help='the name of task')
     parser.add_argument('--model', type=str,
-                        default='STMatching', help='the name of model')
+                        default='GRU', help='the name of model')
     parser.add_argument('--dataset', type=str,
-                        default='00000000', help='the name of dataset')
+                        default='METR_LA', help='the name of dataset')
     parser.add_argument('--config_file', type=str,
                         default=None, help='the file name of config file')
     parser.add_argument('--saved_model', type=str2bool,
