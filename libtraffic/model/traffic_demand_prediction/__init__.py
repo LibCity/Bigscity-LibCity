b625d874127a830dad550a99200aa24a0e037b95
Amadeus <13202127055@163.com>
2021-08-31 15:22:51 +0800

    Optimize directory structure for OD matrix prediction

--- libtraffic/model/traffic_demand_prediction/__init__.py
+++ libtraffic/model/traffic_demand_prediction/__init__.py
@@ -1,11 +1,9 @@
 from libtraffic.model.traffic_demand_prediction.CCRNN import CCRNN
 from libtraffic.model.traffic_demand_prediction.DMVSTNet import DMVSTNet
-from libtraffic.model.traffic_demand_prediction.GEML import GEML
 from libtraffic.model.traffic_demand_prediction.STG2Seq import STG2Seq
 
 __all__ = [
     "STG2Seq",
     "DMVSTNet",
-    "CCRNN",
-    "GEML"
+    "CCRNN"
 ]
