d9ddd36f131e2f1d957f7fdf399b9e3a1383e5eb
XBR-1111 <59074275+XBR-1111@users.noreply.github.com>
2021-10-08 05:53:02 -0500

    add HMMM (#181)

--- libcity/model/map_matching/__init__.py
+++ libcity/model/map_matching/__init__.py
@@ -1,7 +1,8 @@
 from libcity.model.map_matching.STMatching import STMatching
 from libcity.model.map_matching.IVMM import IVMM
-
+from libcity.model.map_matching.HMMM import HMMM
 __all__ = [
     "STMatching",
-    "IVMM"
+    "IVMM",
+    "HMMM"
 ]
