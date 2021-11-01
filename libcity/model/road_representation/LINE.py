d76528c46912c48703910e655e200ffa8cafd754
amadeus <13202127055@163.com>
2021-11-01 11:08:52 +0800

    LINE v0.2

--- libcity/model/road_representation/LINE.py
+++ libcity/model/road_representation/LINE.py
@@ -1,135 +1,40 @@
-from decimal import Decimal
-import random
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from tqdm import tqdm
 
-# Thanks to https://github.com/dmpierre/LINE
-class VoseAlias(object):
-    """
-    Adding a few modifs to https://github.com/asmith26/Vose-Alias-Method
-    """
+from libcity.model.abstract_traffic_state_model import AbstractTrafficStateModel
 
-    def __init__(self, dist):
+
+class LINE(AbstractTrafficStateModel):
+    def __init__(self, config, data_feature):
+        super().__init__(config, data_feature)
+        self.device = config.get('device')
+
+        self.order = config.get('order')
+        self.negative_ratio = config.get('negative_ratio')
+        self.embedding_size = config.get('embedding_size')
+        self.num_nodes = data_feature.get("num_nodes")
+        self.num_edges = data_feature.get("num_edges")
+        self.batch_size = data_feature.get("batch_size")
+
+        self.samples_per_epoch = self.num_edges * (1 + self.negative_ratio)
+
+        self.node_emb = nn.Embedding(self.num_nodes, self.embedding_size)
+
+        if self.order == 'second':
+            self.context_emb = nn.Embedding(self.num_nodes, self.embedding_size)
+        elif self.order != 'first':
+            raise ValueError("order mode must be first or second")
+
+    def forward(self, vi, vj, neg):
         """
-        (VoseAlias, dict) -> NoneType
+        Args:
+            vi : origin vector of node i
+            vj : origin vector of node j
+            neg: origin vectors of nodes neg
         """
-        self.dist = dist
-        self.alias_initialisation()
-
-    def alias_initialisation(self):
-        """
-        Construct probability and alias tables for the distribution.
-        """
-        # Initialise variables
-        n = len(self.dist)
-        self.table_prob = {}  # probability table
-        self.table_alias = {}  # alias table
-        scaled_prob = {}  # scaled probabilities
-        small = []  # stack for probabilities smaller that 1
-        large = []  # stack for probabilities greater than or equal to 1
-
-        # Construct and sort the scaled probabilities into their appropriate stacks
-        print("1/2. Building and sorting scaled probabilities for alias table...")
-        for o, p in tqdm(self.dist.items()):
-            scaled_prob[o] = Decimal(p) * n
-
-            if scaled_prob[o] < 1:
-                small.append(o)
-            else:
-                large.append(o)
-
-        print("2/2. Building alias table...")
-        # Construct the probability and alias tables
-        while small and large:
-            s = small.pop()
-            l = large.pop()
-
-            self.table_prob[s] = scaled_prob[s]
-            self.table_alias[s] = l
-
-            scaled_prob[l] = (scaled_prob[l] + scaled_prob[s]) - Decimal(1)
-
-            if scaled_prob[l] < 1:
-                small.append(l)
-            else:
-                large.append(l)
-
-        # The remaining outcomes (of one stack) must have probability 1
-        while large:
-            self.table_prob[large.pop()] = Decimal(1)
-
-        while small:
-            self.table_prob[small.pop()] = Decimal(1)
-        self.listprobs = list(self.table_prob)
-
-    def alias_generation(self):
-        """
-        Yields a random outcome from the distribution.
-        """
-        # Determine which column of table_prob to inspect
-        col = random.choice(self.listprobs)
-        # Determine which outcome to pick in that column
-        if self.table_prob[col] >= random.uniform(0, 1):
-            return col
+        vi = self.node_emb(vi)
+        if self.order == 'first':
+            vj = self.node_emb(vj)
+            neg = self.node_emb(neg)
         else:
-            return self.table_alias[col]
-
-    def sample_n(self, size):
-        """
-        Yields a sample of size n from the distribution, and print the results to stdout.
-        """
-        for i in range(size):
-            yield self.alias_generation()
-
-
-class LINE(nn.Module):
-    def __init__(self, size, embed_dim=128, order=1):
-        super(LINE, self).__init__()
-
-        assert order in [1, 2], print("Order should either be int(1) or int(2)")
-
-        self.embed_dim = embed_dim
-        self.order = order
-        self.nodes_embeddings = nn.Embedding(size, embed_dim)
-
-        if order == 2:
-            self.contextnodes_embeddings = nn.Embedding(size, embed_dim)
-            # Initialization
-            self.contextnodes_embeddings.weight.data = self.contextnodes_embeddings.weight.data.uniform_(
-                -.5, .5) / embed_dim
-
-        # Initialization
-        self.nodes_embeddings.weight.data = self.nodes_embeddings.weight.data.uniform_(
-            -.5, .5) / embed_dim
-
-    def forward(self, v_i, v_j, negsamples, device):
-
-        v_i = self.nodes_embeddings(v_i).to(device)
-
-        if self.order == 2:
-            v_j = self.contextnodes_embeddings(v_j).to(device)
-            negativenodes = -self.contextnodes_embeddings(negsamples).to(device)
-
-        else:
-            v_j = self.nodes_embeddings(v_j).to(device)
-            negativenodes = -self.nodes_embeddings(negsamples).to(device)
-
-        mulpositivebatch = torch.mul(v_i, v_j)
-        positivebatch = F.logsigmoid(torch.sum(mulpositivebatch, dim=1))
-
-        mulnegativebatch = torch.mul(v_i.view(len(v_i), 1, self.embed_dim), negativenodes)
-        negativebatch = torch.sum(
-            F.logsigmoid(
-                torch.sum(mulnegativebatch, dim=2)
-            ),
-            dim=1)
-        loss = positivebatch + negativebatch
-        return -torch.mean(loss)
-
-    def get_embeddings(self):
-        if self.order == 1:
-            return self.nodes_embeddings.weight.data
-        else:
-            return self.contextnodes_embeddings.weight.data
+            vj = self.context_emb(vj)
+            negs = self.context_emb(neg)
