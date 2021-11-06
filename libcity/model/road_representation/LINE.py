117b131a1ff9afb7642b04b34e942867fbe7974f
amadeus <13202127055@163.com>
2021-11-07 06:18:56 +0800

    LINE v0.8

--- libcity/model/road_representation/LINE.py
+++ libcity/model/road_representation/LINE.py
@@ -1,6 +1,48 @@
 import torch.nn as nn
+import torch.nn.functional as F
 
 from libcity.model.abstract_traffic_state_model import AbstractTrafficStateModel
+
+
+class LINE_FIRST(nn.Module):
+    def __init__(self, num_nodes, embedding_size):
+        super().__init__()
+        self.num_nodes = num_nodes
+        self.embedding_size = embedding_size
+        self.node_emb = nn.Embedding(self.num_nodes, self.embedding_size)
+
+    def forward(self, i, j):
+        """
+        Args:
+            i: indices of i; (B,)
+            j: indices of j; (B,)
+        Return:
+            v_i^T * v_j; (B,)
+        """
+        vi = self.node_emb(i)
+        vj = self.node_emb(j)
+        return (vi * vj).sum(dim=-1)
+
+
+class LINE_SECOND(nn.Module):
+    def __init__(self, num_nodes, embedding_size):
+        super().__init__()
+        self.num_nodes = num_nodes
+        self.embedding_size = embedding_size
+        self.node_emb = nn.Embedding(self.num_nodes, self.embedding_size)
+        self.context_emb = nn.Embedding(self.num_nodes, self.embedding_size)
+
+    def forward(self, I, J):
+        """
+        Args:
+            I: indices of i; (B,)
+            J: indices of j; (B,)
+        Return:
+            [v_i^T * u_j for (i,j) in zip(I,J)]; (B,)
+        """
+        vi = self.node_emb(I)
+        vj = self.context_emb(J)
+        return (vi * vj).sum(dim=-1)
 
 
 class LINE(AbstractTrafficStateModel):
@@ -9,32 +51,31 @@ class LINE(AbstractTrafficStateModel):
         self.device = config.get('device')
 
         self.order = config.get('order')
-        self.negative_ratio = config.get('negative_ratio')
         self.embedding_size = config.get('embedding_size')
         self.num_nodes = data_feature.get("num_nodes")
         self.num_edges = data_feature.get("num_edges")
-        self.batch_size = data_feature.get("batch_size")
 
-        self.samples_per_epoch = self.num_edges * (1 + self.negative_ratio)
-
-        self.node_emb = nn.Embedding(self.num_nodes, self.embedding_size)
-
-        if self.order == 'second':
-            self.context_emb = nn.Embedding(self.num_nodes, self.embedding_size)
-        elif self.order != 'first':
+        if self.order == 'first':
+            self.embed = LINE_FIRST(self.num_nodes, self.embedding_size)
+        elif self.order == 'second':
+            self.embed = LINE_SECOND(self.num_nodes, self.embedding_size)
+        else:
             raise ValueError("order mode must be first or second")
 
-    def forward(self, vi, vj, neg):
+    def calculate_loss(self, batch):
+        I, J, is_neg = batch['I'], batch['J'], batch['Neg']
+        dot_product = self.forward(I, J)
+        return -(F.logsigmoid(dot_product * is_neg)).mean()
+
+    def forward(self, I, J):
         """
         Args:
-            vi : origin vector of node i
-            vj : origin vector of node j
-            neg: origin vectors of nodes neg
+            I : origin indices of node i ; (B,)
+            J : origin indices of node j ; (B,)
+        Return:
+            if order == 'first':
+                [u_j^T * u_i for (i,j) in zip(I, J)]; (B,)
+            elif order == 'second':
+                [u'_j^T * v_i for (i,j) in zip(I, J)]; (B,)
         """
-        vi = self.node_emb(vi)
-        if self.order == 'first':
-            vj = self.node_emb(vj)
-            neg = self.node_emb(neg)
-        else:
-            vj = self.context_emb(vj)
-            negs = self.context_emb(neg)
+        return self.embed(I, J)
