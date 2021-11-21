3671614279f5a5d82ca98e04e477117afcc4bd9b
曲乐皓 <19373528@buaa.edu.cn>
2021-11-21 19:03:12 +0800

    GAT 11/22

--- libcity/model/road_representation/__init__.py
+++ libcity/model/road_representation/__init__.py
@@ -1,7 +1,7 @@
 from libcity.model.road_representation.ChebConv import ChebConv
-from libcity.model.road_representation.LINE import LINE
+from libcity.model.road_representation.GAT import GAT
 
 __all__ = [
     "ChebConv",
-    "LINE"
+    "GAT"
 ]
