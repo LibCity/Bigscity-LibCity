8d4e43ccaa1166c8317f6695a9c491e104c8ba48
ShotDownDiane <532586242@qq.com>
2021-12-16 14:53:11 +0800

    Update utils.py

--- libcity/layer/utils.py
+++ libcity/layer/utils.py
@@ -1,9 +1,12 @@
+import math
 import torch
+import torch.nn.functional as F
 import torch.nn as nn
 import numpy as np
 from scipy.sparse.linalg import eigs
 from Output.mlp import FC
-
+import scipy.sparse as sp
+from scipy.sparse import linalg
 
 def cheb_polynomial(l_tilde, k):
     """
@@ -48,7 +51,6 @@ def scaled_laplacian(weight):
 
 
 class GatedFusion(nn.Module):
-
     def __init__(self, dim, bn, bn_decay, device):
         super(GatedFusion, self).__init__()
         self.D = dim
@@ -63,20 +65,122 @@ class GatedFusion(nn.Module):
                             bn=self.bn, bn_decay=self.bn_decay, device=self.device)
 
     def forward(self, HS, HT):
-        """
+        '''
         gated fusion
-
-        Args:
-            HS:     (batch_size, num_step, num_nodes, D)
-            HT:     (batch_size, num_step, num_nodes, D)
-
-        Returns:
-            tensor: (batch_size, num_step, num_nodes, D)
-
-        """
+        HS:     (batch_size, num_step, num_nodes, D)
+        HT:     (batch_size, num_step, num_nodes, D)
+        return: (batch_size, num_step, num_nodes, D)
+        '''
         XS = self.HS_fc(HS)
         XT = self.HT_fc(HT)
         z = torch.sigmoid(torch.add(XS, XT))
         H = torch.add(torch.multiply(z, HS), torch.multiply(1 - z, HT))
         H = self.output_fc(H)
         return H
+
+def calculate_normalized_laplacian(adj):
+    """
+    L = D^-1/2 (D-A) D^-1/2 = I - D^-1/2 A D^-1/2
+
+    Args:
+        adj: adj matrix
+
+    Returns:
+        np.ndarray: L
+    """
+    adj = sp.coo_matrix(adj)
+    d = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(d, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    normalized_laplacian = sp.eye(adj.shape[0]) - adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).tocoo()
+    return normalized_laplacian
+
+
+def calculate_random_walk_matrix(adj_mx):
+    adj_mx = sp.coo_matrix(adj_mx)
+    d = np.array(adj_mx.sum(1))
+    d_inv = np.power(d, -1).flatten()
+    d_inv[np.isinf(d_inv)] = 0.
+    d_mat_inv = sp.diags(d_inv)
+    random_walk_mx = d_mat_inv.dot(adj_mx).tocoo()
+    return random_walk_mx
+
+
+def calculate_reverse_random_walk_matrix(adj_mx):
+    return calculate_random_walk_matrix(np.transpose(adj_mx))
+
+
+def calculate_scaled_laplacian(adj_mx, lambda_max=2, undirected=True):
+    if undirected:
+        adj_mx = np.maximum.reduce([adj_mx, adj_mx.T])
+    lap = calculate_normalized_laplacian(adj_mx)
+    if lambda_max is None:
+        lambda_max, _ = linalg.eigsh(lap, 1, which='LM')
+        lambda_max = lambda_max[0]
+    lap = sp.csr_matrix(lap)
+    m, _ = lap.shape
+    identity = sp.identity(m, format='csr', dtype=lap.dtype)
+    lap = (2 / lambda_max * lap) - identity
+    return lap.astype(np.float32)
+
+
+def calculate_cheb_poly(lap, ks):
+    """
+    k-order Chebyshev polynomials : T0(L)~Tk(L)
+    T0(L)=I/1 T1(L)=L Tk(L)=2LTk-1(L)-Tk-2(L)
+
+    Args:
+        lap: scaled laplacian matrix
+        ks: k-order
+
+    Returns:
+        np.ndarray: T0(L)~Tk(L)
+    """
+    n = lap.shape[0]
+    lap_list = [np.eye(n), lap[:]]
+    for i in range(2, ks):
+        lap_list.append(np.matmul(2 * lap, lap_list[-1]) - lap_list[-2])
+    if ks == 0:
+        raise ValueError('Ks must bigger than 0!')
+    if ks == 1:
+        return np.asarray(lap_list[0:1])  # 1*n*n
+    else:
+        return np.asarray(lap_list)       # Ks*n*n
+
+
+def calculate_first_approx(weight):
+    '''
+    1st-order approximation function.
+    :param W: weighted adjacency matrix of G. Not laplacian matrix.
+    :return: np.ndarray
+    '''
+    # TODO: 如果W对角线本来就是全1？
+    n = weight.shape[0]
+    adj = weight + np.identity(n)
+    d = np.sum(adj, axis=1)
+    # sinvd = np.sqrt(np.mat(np.diag(d)).I)
+    # return np.array(sinvd * A * sinvd)
+    sinvd = np.sqrt(np.linalg.inv(np.diag(d)))
+    lap = np.matmul(np.matmul(sinvd, adj), sinvd)  # n*n
+    lap = np.expand_dims(lap, axis=0)              # 1*n*n
+    return lap
+
+
+def sym_adj(adj):
+    """Symmetrically normalize adjacency matrix."""
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1))
+    d_inv_sqrt = np.power(rowsum, -0.5).flatten()
+    d_inv_sqrt[np.isinf(d_inv_sqrt)] = 0.
+    d_mat_inv_sqrt = sp.diags(d_inv_sqrt)
+    return adj.dot(d_mat_inv_sqrt).transpose().dot(d_mat_inv_sqrt).astype(np.float32).todense()
+
+
+def asym_adj(adj):
+    adj = sp.coo_matrix(adj)
+    rowsum = np.array(adj.sum(1)).flatten()
+    d_inv = np.power(rowsum, -1).flatten()
+    d_inv[np.isinf(d_inv)] = 0.
+    d_mat = sp.diags(d_inv)
+    return d_mat.dot(adj).astype(np.float32).todense()
