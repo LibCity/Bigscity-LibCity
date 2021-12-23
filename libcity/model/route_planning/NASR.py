b78beec28ca92981f5c5a86779bc6edbb3b8b97a
MellesJ <842595644@qq.com>
2021-12-23 16:29:11 +0800

    finish NASR model

--- libcity/model/route_planning/NASR.py
+++ libcity/model/route_planning/NASR.py
@@ -1,7 +1,11 @@
+from queue import PriorityQueue
 import torch
 import torch.nn as nn
+import numpy as np
+from geopy import distance
 from libcity.model.abstract_model import AbstractModel
 from libcity.model.road_representation.GAT import GATLayerImp3
+from libcity.data.dataset.nasr_dataset import distance_to_bin
 
 
 class Attention(nn.Module):
@@ -68,7 +72,7 @@ class FunctionG(nn.Module):
         # score linear
         self.score_lr = nn.Linear(in_features=self.hidden_size, out_features=1, bias=False)
 
-    def forward(self, uid, current_trace, history_trace, candidate_set):
+    def forward(self, uid, current_trace, history_trace, candidate_set, history_trace_hidden=None):
         """
         The FunctionG cannot parallel calculate, which means the batch_size may not work in this function.
         The FunctionG may cost a lot of time.
@@ -84,6 +88,7 @@ class FunctionG(nn.Module):
             history_trace (list): history_len list of history_trace. the list size will be
                                   (history_len, trace_len, 3). no padding.
             candidate_set (list): list of candidate set tensor (candidate_size). no padding.
+            history_trace_hidden (tensor): already encoded history trace hidden. this param will be used in prediction.
 
         Returns:
             candidate_g_prob (list): list of each candidate prob in each step of a trace.
@@ -93,21 +98,24 @@ class FunctionG(nn.Module):
         moving_state = []
         # calculate function g for each step in trace
         uid = uid.unsqueeze(0)  # (1)
-        # encode history trace
-        history_trace_hidden_list = []
-        for history_j in range(len(history_trace)):
-            # (1, trace_len, 3)
-            history_trace_j = torch.LongTensor(history_trace[history_j]).to(self.device).unsqueeze(0)
-            history_trace_j_hidden = self.encode_trace(uid, history_trace_j)  # (hidden_size)
-            history_trace_hidden_list.append(history_trace_j_hidden.unsqueeze(0))
-        # form list to tensor
-        # (history_len, hidden_size)
-        history_trace_hidden = torch.cat(history_trace_hidden_list, dim=0)
+        if history_trace_hidden is not None:
+            # encode history trace
+            history_trace_hidden_list = []
+            for history_j in range(len(history_trace)):
+                # (1, trace_len, 3)
+                history_trace_j = torch.LongTensor(history_trace[history_j]).to(self.device).unsqueeze(0)
+                history_trace_j_hidden = self.encode_trace(uid, history_trace_j)  # (hidden_size)
+                history_trace_hidden_list.append(history_trace_j_hidden.unsqueeze(0))
+            # form list to tensor
+            # (history_len, hidden_size)
+            history_trace_hidden = torch.cat(history_trace_hidden_list, dim=0)
         for step_i in range(len(current_trace)):
             # calculate each candidate's hidden
             # append each candidate to current_trace
             current_trace_i = current_trace[step_i]  # (trace_len, 3)
+            current_trace_i = torch.LongTensor(current_trace_i).to(self.device)
             candidate_set_i = candidate_set[step_i]  # (candidate_size)
+            candidate_set_i = torch.LongTensor(candidate_set_i).to(self.device)
             # (candidate_size, trace_len, 3)
             current_trace_i = current_trace_i.unsqueeze(0).repeat(candidate_set_i.shape[0], 1, 1)
             candidate_set_i_time = current_trace_i[:, -1, 1:]  # (candidate_size, 2)
@@ -331,9 +339,25 @@ class FunctionH(nn.Module):
         fc2_out = self.fc2(fc1_out)
         return fc2_out
 
-    def calculate_loss(self, moving_state, li, ld, distance, target):
-        pred_h = self.forward(moving_state, li, ld, distance)
-        return torch.norm(pred_h - target)
+
+class SearchNode(object):
+    """
+    The search node will save the context information of the search branch of the node
+    """
+
+    def __init__(self, trace, rid, date_time, log_prob):
+        """
+        Args:
+            trace: the trace of the search node branch, (trace_len, 3)
+            rid: the search node's rid
+            date_time: The time object of this node is the number of minutes from zero.
+                       This time will be used to calculate the next node's time.
+            log_prob:  the log probability log(P(l_s -> l_i | l_s, l_d, t_s))
+        """
+        self.trace = trace
+        self.rid = rid
+        self.date_time = date_time
+        self.log_prob = log_prob
 
 
 class NASR(AbstractModel):
@@ -344,32 +368,168 @@ class NASR(AbstractModel):
 
     def __init__(self, config, data_feature):
         super(NASR, self).__init__(config, data_feature)
+        self.time_diff_n = config['time_diff_n']
+        self.discount_rate = config['discount_rate']
+        self.road_gps = data_feature['road_gps']
+        self.device = config['device']
         self.function_g = FunctionG(config, data_feature)
         self.function_h = FunctionH(config, data_feature)
 
-    def forward(self, uid, current_trace, history_trace, candidate_set):
+    def forward(self, uid, current_trace, history_hidden, candidate_set, candidate_distance, des):
         """
 
         Args:
-            uid (batch_size): the uid of this trace.
-            current_trace (list): batch_size list of current_trace tensor (trace_len, 3),
+            uid (1): the uid of this trace.
+            current_trace (tensor): current_trace tensor (trace_len, 3),
                                   the feature dimension is [location id, weekday id, hour id]
-            history_trace (list): batch_size list of history_trace. the list size will be
-                                  (batch_size, history_len, trace_len, 3). no padding.
-            candidate_set (list): batch_size list of candidate set tensor (candidate_size). no padding.
+            history_hidden (tensor): the encoded history hidden by function g.
+            candidate_set (tensor): candidate set tensor (candidate_size). no padding.
+            candidate_distance (tensor): the distance between candidate and destination.
+            des (tensor): the destination rid.
 
         Returns:
-
+            f_cost: each candidate's f_cost
         """
+        # calculate g prob
+        g_prob, moving_state = self.function_g.forward(uid=uid, current_trace=current_trace,
+                                                       candidate_set=candidate_set,
+                                                       history_trace=[], history_trace_hidden=history_hidden)
+        # This step needs to be done because of the function_g forward interface
+        g_prob = g_prob[0]
+        moving_state = moving_state[0]
+        # calculate h cost for each
+        h_cost = torch.zeros(moving_state.shape[0]).to(self.device)
+        for i in range(moving_state.shape[0]):
+            h_cost[i] = self.function_h.forward(moving_state=moving_state[i], li=candidate_set[i],
+                                                ld=des, distance=candidate_distance[i])
+        g_cost = -torch.log(g_prob)
+        f_cost = g_cost + h_cost
+        return f_cost
 
     def predict(self, batch):
         """
         Args:
-            batch (Batch): a batch of input
+            batch (Batch): a batch of queries. A query is composed of (l_s, weekday_s, hour_s, l_d, uid, history_trace)
 
         Returns:
-            torch.tensor: predict result of this batch
+            generate_trace (list): list of NASR recommend traces, only contains road list and uid. (uid, generate_trace)
         """
+        # use A* search
+        query = batch['query']
+        generate_trace = []
+        for i in range(len(query)):
+            # parse query
+            query_i = query[i]
+            des_rid = query_i[3]
+            uid = torch.LongTensor(query_i[4]).to(self.device)
+            open_set = PriorityQueue()  # unvisited node set
+            close_set = set()  # visited node set
+            rid2node = {}  # the dict key is rid, and value is corresponding search node
+            des_center_gps = self.road_gps[str(des_rid)]
+            des = torch.LongTensor([des_rid]).to(self.device)
+            # put l_s into open_set
+            start_node = SearchNode(trace=[query_i[0], query_i[1], query_i[2]], rid=query_i[0],
+                                    date_time=query_i[2] * 60, log_prob=0.0)
+            open_set.put((start_node.log_prob, start_node))
+            rid2node[query_i[0]] = start_node
+            max_search_step = 500
+            step = 0
+            # encode history
+            history_trace_i = query_i[5]
+            history_trace_hidden_list = []
+            for history_j in range(len(history_trace_i)):
+                # (1, trace_len, 3)
+                history_trace_j = torch.LongTensor(history_trace_i[history_j]).to(self.device).unsqueeze(0)
+                history_trace_j_hidden = self.function_g.encode_trace(uid, history_trace_j)  # (hidden_size)
+                history_trace_hidden_list.append(history_trace_j_hidden.unsqueeze(0))
+            # form list to tensor
+            # (history_len, hidden_size)
+            history_trace_hidden = torch.cat(history_trace_hidden_list, dim=0)
+            # find flag
+            find_flag = False
+            best_trace = []
+            best_score = 0
+            default_len = 15
+            while not open_set.empty() and step < max_search_step:
+                # get min f cost node to search
+                cost, now_node = open_set.get()
+                # put now node in close_set
+                now_rid = now_node.rid
+                if now_rid in close_set:
+                    # now_node is an old node
+                    # now_rid has been visited
+                    continue
+                close_set.add(now_rid)
+                # del now_id from rid2node, because now node has been visited
+                rid2node.pop(now_rid, None)
+                if now_rid == des_rid:
+                    # find destination
+                    trace_i = now_node.trace
+                    best_trace = [x[0] for x in trace_i]
+                    # finish search for query_i
+                    find_flag = True
+                    break
+                else:
+                    # search now's adjacent rid
+                    candidate_set = self.gat.adj_mx.getrow(now_rid).tolist()
+                    if len(candidate_set) != 0:
+                        candidate_dis = []
+                        for c in candidate_set:
+                            candidate_gps = self.road_gps[str(c)]
+                            d = distance.distance((des_center_gps[1], des_center_gps[0]),
+                                                  (candidate_gps[1], candidate_gps[0])).kilometers * 1000
+                            candidate_dis.append(distance_to_bin(d))
+                        trace = torch.LongTensor(now_node.trace).to(self.device)
+                        candidate = torch.LongTensor(candidate_set).to(self.device)
+                        dis = torch.FloatTensor(candidate_dis).to(self.device)
+                        # 根据模型计算 f 函数值和 g 函数值
+                        with torch.no_grad():
+                            candidate_f_cost = self.forward(uid=uid, current_trace=trace,
+                                                            history_hidden=history_trace_hidden,
+                                                            candidate_set=candidate, candidate_distance=dis,
+                                                            des=des)
+                        # update each candidate's node
+                        # calculate next datetime
+                        now_datetime = now_node.date_time
+                        now_weekday = now_node.trace[-1][1]
+                        # add a fix time step
+                        next_datetime = now_datetime + 5
+                        if next_datetime >= 1440:
+                            # a new day
+                            # weekday from 0 to 6
+                            now_weekday = (now_weekday + 1) % 7
+                            next_datetime = next_datetime % 1440
+                        next_hourindex = next_datetime // 60
+                        for index, c in enumerate(candidate_set):
+                            candidate_log_prob = now_node.log_prob + candidate_f_cost[index].item()
+                            candidate_score = np.exp(-candidate_log_prob)
+                            if c not in rid2node and c not in close_set:
+                                # c has not been reached
+                                candidate_node = SearchNode(trace=now_node.trace + [[c, now_weekday, next_hourindex]],
+                                                            rid=c, date_time=next_datetime, log_prob=candidate_log_prob)
+                                # put c in open_set
+                                open_set.put((candidate_log_prob, candidate_node))
+                                rid2node[c] = candidate_node
+                                if len(candidate_node.trace) == default_len and candidate_score > best_score:
+                                    # give the default recommended trace
+                                    best_trace = candidate_node.trace
+                                    best_score = candidate_score
+                            elif c in rid2node and rid2node[c].log_prob > candidate_log_prob:
+                                # update search node
+                                rid2node[c].log_prob = candidate_log_prob
+                                rid2node[c].trace = now_node.trace + [[c, now_weekday, next_hourindex]]
+                                rid2node[c].date_time = next_datetime
+                                # there seems no way to update c in open_set, so just put c in open_set.
+                                # the higher priority c will be searched first.
+                                # so this is still work.
+                                open_set.put((candidate_log_prob, rid2node[c]))
+                                if len(rid2node[c].trace) == default_len and candidate_score > best_score:
+                                    # give the default recommended trace
+                                    best_trace = rid2node[c].trace
+                                    best_score = candidate_score
+                step += 1
+            generate_trace.append((uid, best_trace))
+        return generate_trace
 
     def calculate_loss(self, batch):
         """
@@ -391,21 +551,45 @@ class NASR(AbstractModel):
             history_trace = batch['history_trace']
             candidate_set = batch['candidate_set']
             target = batch['target']
+            candidate_distance = batch['distance']  # list
             for iter_i in range(uid.shape[0]):
                 # one trace one calculate
                 uid_i = uid[iter_i]
                 target_g_prob = []
-                target_moving_state = []
-                candidate_g_prob, moving_state = self.function_g.forward(uid_i, current_trace, history_trace,
-                                                                         candidate_set)
+                target_h_cost = []
+                with torch.no_grad():
+                    # when train h, g should be ground truth.
+                    candidate_g_prob, moving_state = self.function_g.forward(uid_i, current_trace[iter_i],
+                                                                             history_trace[iter_i],
+                                                                             candidate_set[iter_i])
                 # select target's prob and moving state
+                des_index = target[iter_i][-1]
+                ld = candidate_set[iter_i][-1][des_index]
                 for target_i in range(len(target[iter_i])):
+                    # the last point do not need to learn
                     target_index = target[iter_i][target_i]
-                    target_g_prob.append(candidate_g_prob[target_i][target_index].item())
-                    target_moving_state.append(moving_state[target_i][target_index])
+                    li = torch.LongTensor([candidate_set[iter_i][target_i][target_index]])  # (1)
+                    distance_li_ld = torch.LongTensor([candidate_distance[iter_i][target_i]])  # (1)
+                    target_i_h_cost = self.function_h.forward(moving_state=moving_state[target_i][target_index], li=li,
+                                                              ld=ld, distance=distance_li_ld)
+                    target_g_prob.append(candidate_g_prob[target_i][target_index])
+                    target_h_cost.append(target_i_h_cost)
                 # base on target_g_prob calculate function h's target
-                # set n = 5
-                # calculate each step function h's value
-
+                target_h_cost = torch.cat(target_h_cost, dim=0)  # (trace_len)
+                target_g_prob = torch.cat(target_g_prob, dim=0)  # (trace_len)
+                target_g_cost = - torch.log(target_g_prob)
+                # set n = min(time_diff_n, trace_len - 1)
+                n = min(self.time_diff_n, target_h_cost.shape[0] - 1)
+                pre_h_n = target_h_cost[:-n]
+                after_h_n = target_h_cost[n:]
+                discount_g = []
+                for i in range(pre_h_n.shape[0]):
+                    c_lj = target_g_cost[i+1].clone()
+                    for j in range(i+2, i+n):
+                        c_lj += self.discount_rate ** (j-i-1) * target_g_cost[j]
+                    discount_g.append(c_lj)
+                discount_g = torch.cat(discount_g, dim=0)
+                loss = torch.sum(torch.square(pre_h_n - self.discount_rate ** n * after_h_n - discount_g))
+                return loss
         else:
             raise NotImplementedError('the loss mode {} is not supported by NASR'.format(loss_mode))
