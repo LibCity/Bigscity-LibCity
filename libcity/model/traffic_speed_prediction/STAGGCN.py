ca37bb3205a3c9bedc17e3940a7970b822bf9d17
Amadeus <59809602+l782993610@users.noreply.github.com>
2021-12-25 20:41:20 +0800

    [Code]Disable the import of torch_scatter(Temp)

--- libcity/model/traffic_speed_prediction/STAGGCN.py
+++ libcity/model/traffic_speed_prediction/STAGGCN.py
@@ -7,7 +7,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils import weight_norm
-from torch_scatter import scatter  # NB : Install this package manually
+# from torch_scatter import scatter  # NB : Install this package manually
 
 from libcity.model import loss
 from libcity.model.abstract_traffic_state_model import AbstractTrafficStateModel
