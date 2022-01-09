fc5631ee4cf0d2ee090458ff7114e53bfbf0f927
nadiaaaaachen <17706413109@163.com>
2022-01-09 17:47:14 +0800

    MPR数据处理

--- libcity/data/dataset/mostpopularroute_dataset.py
+++ libcity/data/dataset/mostpopularroute_dataset.py
@@ -1,54 +1,24 @@
-import copy
 import os
 import json
 import pandas as pd
 import math
 import scipy.sparse as sp
 from tqdm import tqdm
-from datetime import datetime
+import random
 from logging import getLogger
 from libcity.data.dataset.abstract_dataset import AbstractDataset
 from libcity.data.list_dataset import ListDataset
-from libcity.data.batch import Batch
 from torch.utils.data import DataLoader
 
-from libcity.utils.transfer_probability import TransferProbability
+parameter_list = ['dataset', 'train_rate']
 
-parameter_list = ['dataset', 'train_rate', 'eval_rate']
-
-
-def distance_to_bin(distance_x):
-    """
-    discrete distance between road
-    The bin size is 500 m.
-    For distances over 10 km, all are mapped into a same bucket.
-
-    Args:
-        distance_x: the unit is meter.
-
-    Returns:
-        distance_bin (int)
-    """
-    if distance_x >= 10000:
-        return 20
-    else:
-        return int(distance_x // 500)
-
-
-def encode_time(timestamp):
-    """
-    encode a timestamp to weekday index and hour index
-    Args:
-        timestamp: the string of time
-
-    Returns:
-        weekday_index, hour_index
-    """
-    date = datetime.strptime(timestamp, '%Y-%m-%dT%H:%M:%SZ')
-    weekday_index = date.weekday()
-    hour_index = date.hour
-    return weekday_index, hour_index
-
+def random_dict(dict):
+    dict_key = list(dict.keys())
+    random.shuffle(dict_key)
+    new_dict = {}
+    for key in dict_key:
+        new_dict[key] = dict.get(key)
+    return new_dict
 
 class MPRDataset(AbstractDataset):
 
@@ -56,7 +26,7 @@ class MPRDataset(AbstractDataset):
         self.config = config
         self.cache_filename = './libcity/cache/dataset_cache/MPRData'
         for param in parameter_list:
-            if param == 'train_rate' or param == 'eval_rate':
+            if param == 'train_rate':
                 self.cache_filename += '_' + str(int(self.config[param] * 100))
             else:
                 self.cache_filename += '_' + str(self.config[param])
@@ -67,78 +37,56 @@ class MPRDataset(AbstractDataset):
         self.adj_mx = None
         self.adjacent_list = None
         self.road_gps = None
-        self.uid_num = None
         self.road_num = None
+        self.traj_num = None
         self._logger = getLogger()
 
     def get_data(self):
         """
-        返回数据的DataLoader，包括训练数据、测试数据、验证数据
+        返回数据的DataLoader，包括训练数据、验证数据
 
         Returns:
             tuple: tuple contains:
                 train_dataloader: Dataloader composed of Batch (class)
-                eval_dataloader: Dataloader composed of Batch (class)
                 test_dataloader: Dataloader composed of Batch (class)
         """
         if self.data is None:
-            if self.config['cache_dataset'] and os.path.exists(self.cache_filename):
+            if os.path.exists(self.cache_filename):
                 # load cache
                 self._logger.info('use cache data')
                 with open(self.cache_filename, 'r') as f:
                     cache_data = json.load(f)
                     self.data = cache_data['data']
-                    self.uid_num = cache_data['uid_num']
+                    self.traj_num = cache_data['traj_num']
                     self.load_roadmap()
                     train_data = self.data['train_data']
-                    eval_data = self.data['eval_data']
                     test_data = self.data['test_data']
             else:
                 # first load road map infomation
                 self.load_roadmap()
                 # second read trajectory data
-                train_data, eval_data, test_data = self.load_dyna()
+                train_data, test_data = self.load_dyna()
                 # cache data
                 self.data = {
                     'train_data': train_data,
-                    'eval_data': eval_data,
                     'test_data': test_data,
                 }
-                # node_features and adj_mx cannot save in json
+                # adj_mx cannot save in json
                 with open(self.cache_filename, 'w') as f:
                     json.dump({
                         'data': self.data,
-                        'uid_num': self.uid_num,
+                        'traj_num': self.traj_num,
                     }, f)
-        self._logger.info('Train data {}, Eval data {}, Test data {}'.format(len(train_data), len(eval_data),
-                                                                             len(test_data)))
+        self._logger.info('Train data {}, Test data {}'.format(len(train_data), len(test_data)))
         # generate dataloader
         train_dataset = ListDataset(train_data)
-        eval_dataset = ListDataset(eval_data)
         test_dataset = ListDataset(test_data)
 
-        def collator_train(indices):
-            batch = Batch(self.train_feature_name, {}, {})
-            for item in indices:
-                batch.append(copy.deepcopy(item))
-            return batch
-
-        def collator_test(indices):
-            batch = Batch(self.test_feature_name, {}, {})
-            for item in indices:
-                batch.append(copy.deepcopy(item))
-            return batch
-
         train_dataloader = DataLoader(dataset=train_dataset, batch_size=self.config['batch_size'],
-                                      num_workers=self.config['num_workers'], collate_fn=collator_train,
-                                      shuffle=False)
-        eval_dataloader = DataLoader(dataset=eval_dataset, batch_size=self.config['batch_size'],
-                                     num_workers=self.config['num_workers'], collate_fn=collator_train,
-                                     shuffle=False)
+                                      num_workers=self.config['num_workers'], shuffle=False)
         test_dataloader = DataLoader(dataset=test_dataset, batch_size=self.config['batch_size'],
-                                     num_workers=self.config['num_workers'], collate_fn=collator_test,
-                                     shuffle=False)
-        return train_dataloader, eval_dataloader, test_dataloader
+                                      num_workers=self.config['num_workers'], shuffle=False)
+        return train_dataloader, test_dataloader
 
     def get_data_feature(self):
         """
@@ -148,11 +96,10 @@ class MPRDataset(AbstractDataset):
             dict: 包含数据集的相关特征的字典
         """
         return {
-            'uid_num': self.uid_num,
             'loc_num': self.road_num,
-            'adj_mx': self.adj_mx,
-            'distance_bins': 21,
             'road_gps': self.road_gps,
+            'traj_num': self.traj_num,
+            'adj_mx': self.adj_mx,
             'adjacent_list': self.adjacent_list
         }
 
@@ -199,101 +146,55 @@ class MPRDataset(AbstractDataset):
 
     def load_dyna(self):
         """
-        load dyna file. generate train data, eval data and test data
+        load dyna file. generate train data and test data
         Returns:
-            train_data, eval_data, test_data (list)
+            train_data, test_data (list)
         """
         # first read dyna, and group the trajectory by entity_id and traj_id
-        user_traj = {}
+        traj = {}
         with open(os.path.join(self.data_path, self.dyna_filename), 'r') as f:
             # read head
             f.readline()
             for line in tqdm(f, desc='group dyna file by entity_id and traj_id'):
-                # dyna_id, type, timestamp, entity_id, traj_id, location id
+                # dyna_id, type, entity_id, traj_id, location id
                 items = line.split(',')
-                timestamp = items[2]
                 entity_id = items[3]
                 traj_id = items[4]
                 location_id = int(items[5])
-                weekday_index, hour_index = encode_time(timestamp)
-                if entity_id not in user_traj:
-                    user_traj[entity_id] = {traj_id: [[location_id, weekday_index, hour_index]]}
-                elif traj_id not in user_traj[entity_id]:
-                    user_traj[entity_id][traj_id] = [[location_id, weekday_index, hour_index]]
+                if (entity_id, traj_id) not in list(traj.keys()):
+                    traj[(entity_id, traj_id)] = []
+                    traj[(entity_id, traj_id)].append(location_id)
                 else:
-                    user_traj[entity_id][traj_id].append([location_id, weekday_index, hour_index])
-        # encode trajectory for each user
+                    traj[(entity_id, traj_id)].append(location_id)
+        # encode trajectory
         train_data = []
-        eval_data = []
         test_data = []
-        self.uid_num = len(user_traj)
-        # encode uid
-        new_uid = 0
-        for uid in tqdm(user_traj, total=len(user_traj), desc='encode traj for each user'):
-            sub_train_data, sub_eval_data, sub_test_data = self.encode_traj(new_uid, user_traj[uid])
+        self.traj_num = len(traj)
+        # encode traj
+        traj = random_dict(traj)
+        new_traj = 0
+        for t in tqdm(traj, total=len(traj), desc='encode traj'):
+            sub_train_data, sub_test_data = self.encode_traj(new_traj, traj[t])
             train_data.extend(sub_train_data)
-            eval_data.extend(sub_eval_data)
             test_data.extend(sub_test_data)
-            new_uid += 1
-        return train_data, eval_data, test_data
+            new_traj += 1
+        return train_data, test_data
 
-    def encode_traj(self, uid, trajectory):
+    def encode_traj(self, new_traj, trajectory):
         """
-        Because we divide data by user, we can divide data when we encode data.
         Args:
-            uid (int): the uid of user
-            trajectory (dict): the dict of a user's trajectory. {traj_id: list of a trajectory}.
+            new_traj (int): the index of trajectory
+            trajectory (list): the dict of a trajectory.
 
         Returns:
-            train_data, eval_data, test_data
+            train_data, test_data
         """
         train_data = []
-        eval_data = []
         test_data = []
-        total_cnt = len(trajectory)
-        train_index = math.floor(total_cnt * self.config['train_rate'])
-        eval_index = train_index + math.floor(total_cnt * self.config['eval_rate'])
-        trajectories = dict()
-        traj_dict = {}
-        traj_set = set()
-        for index, traj_id in enumerate(trajectory):
-            if traj_id not in trajectories.keys():
-                trajectories[traj_id] = []
-            else:
-                trajectories[traj_id].pop()
-            current_trace = trajectory[traj_id]
-            trajectories[traj_id].append(current_trace[0][0])
-            if index < train_index:
-                # now generate train data
-                for step in range(len(current_trace) - 1):
-                    f_id = current_trace[step][0]
-                    t_id = current_trace[step + 1][0]
-                    trajectories[traj_id].append(t_id)
-                    if (f_id, t_id) not in traj_set:
-                        traj_set.add((f_id, t_id))
-                        traj_dict[(f_id, t_id)] = [traj_id]
-                    else:
-                        traj_dict[(f_id, t_id)].append(traj_id)
-                t0 = TransferProbability(self.road_gps, traj_dict, trajectories)
-                train_data.append(t0.p)
-            elif index < eval_index:
-                # now generate eval data.
-                for step in range(len(current_trace) - 1):
-                    f_id = current_trace[step][0]
-                    t_id = current_trace[step + 1][0]
-                    trajectories[traj_id].append(t_id)
-                    if (f_id, t_id) not in traj_set:
-                        traj_set.add((f_id, t_id))
-                        traj_dict[(f_id, t_id)] = [traj_id]
-                    else:
-                        traj_dict[(f_id, t_id)].append(traj_id)
-                t1 = TransferProbability(self.road_gps, traj_dict, trajectories)
-                eval_data.append(t1.p)
-                pass
-            else:
-                # now generate test data.
-                # test data will need: query, truth
-                # query is composed of (l_s, weekday_s, hour_s, l_d, uid, history_trace)
-
-                pass
-        return train_data, eval_data, test_data
+        train_index = math.floor(self.traj_num * self.config['train_rate'])
+        if new_traj < train_index:
+            train_data.append(trajectory)
+        else:
+            # now generate test data.
+            test_data.append(trajectory)
+        return train_data, test_data
---
+++ libcity/model/route_planning/mostpopularroute.py
@@ -0,0 +1,54 @@
+from libcity.model.abstract_machine_learning_model import AbstractMachineLearningModel
+from libcity.utils.transfer_probability import TransferProbability
+
+class MPR(AbstractMachineLearningModel):
+    def __init__(self, config, data_feature):
+        """
+
+        Args:
+            config (ConfigParser): the dict of config
+            data_feature (dict): the dict of data feature passed from Dataset.
+        """
+        super().__init__(config, data_feature)
+        self.road_gps = self.data_feature.get('road_gps')
+        self.road_num = self.data_feature.get('loc_num')
+        self.traj_num = self.data_feature.get('traj_num')
+        self.adj_mx = self.data_feature.get('adj_mx')
+
+        self.transferpro_mx = None
+
+    def predict(self, batch):
+        """
+        Args:
+            batch (Batch): a batch of input
+
+        Returns:
+            result (object) : predict result of this batch
+        """
+
+    def fit(self, batch):
+        """
+        use train data to fit the machine learning model
+
+        Args:
+            batch (Batch): a batch of input. Generally speaking, the train data of machine learning method
+            does not need to be divided into batches, just use Batch to pass in all the data directly.
+
+        Returns:
+            return None
+        """
+        trajectories = dict()
+        traj_set = set()
+        traj_dict = {}
+        for traj_id, trace in enumerate(batch):
+            current_trace = batch[traj_id]
+            trajectories[traj_id] = current_trace
+            for step in range(len(current_trace) - 1):
+                f_id = current_trace[step]
+                t_id = current_trace[step + 1]
+                if (f_id, t_id) not in traj_set:
+                    traj_set.add((f_id, t_id))
+                    traj_dict[(f_id, t_id)] = [traj_id]
+                else:
+                    traj_dict[(f_id, t_id)].append(traj_id)
+        self.transferpro_mx = TransferProbability(self.road_gps, traj_dict, trajectories)
