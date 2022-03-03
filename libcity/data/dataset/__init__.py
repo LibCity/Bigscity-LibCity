6dc6a213e3f32c7de3196b4fae2343b3f991f285
aptx1231 <jwjiang_buaa@163.com>
2022-03-03 19:52:41 +0800

    fix init

--- libcity/data/dataset/__init__.py
+++ libcity/data/dataset/__init__.py
@@ -1,18 +1,12 @@
 from libcity.data.dataset.abstract_dataset import AbstractDataset
 from libcity.data.dataset.trajectory_dataset import TrajectoryDataset
 from libcity.data.dataset.traffic_state_datatset import TrafficStateDataset
-from libcity.data.dataset.traffic_state_cpt_dataset import \
-    TrafficStateCPTDataset
-from libcity.data.dataset.traffic_state_point_dataset import \
-    TrafficStatePointDataset
-from libcity.data.dataset.traffic_state_grid_dataset import \
-    TrafficStateGridDataset
-from libcity.data.dataset.traffic_state_grid_od_dataset import \
-    TrafficStateGridOdDataset
+from libcity.data.dataset.traffic_state_cpt_dataset import TrafficStateCPTDataset
+from libcity.data.dataset.traffic_state_point_dataset import TrafficStatePointDataset
+from libcity.data.dataset.traffic_state_grid_dataset import TrafficStateGridDataset
+from libcity.data.dataset.traffic_state_grid_od_dataset import TrafficStateGridOdDataset
 from libcity.data.dataset.traffic_state_od_dataset import TrafficStateOdDataset
 from libcity.data.dataset.eta_dataset import ETADataset
-from libcity.data.dataset.dataset_subclass.gts_dataset import GTSDataset
-from libcity.data.dataset.dataset_subclass.pbs_trajectory_dataset import PBSTrajectoryDataset
 from libcity.data.dataset.map_matching_dataset import MapMatchingDataset
 from libcity.data.dataset.roadnetwork_dataset import RoadNetWorkDataset
 
@@ -26,28 +20,6 @@ __all__ = [
     "TrafficStateOdDataset",
     "TrafficStateGridOdDataset",
     "ETADataset",
-    "ACFMDataset",
-    "TGCLSTMDataset",
-    "ASTGCNDataset",
-    "STResNetDataset",
-    "STG2SeqDataset",
-    "PBSTrajectoryDataset",
-    "GMANDataset",
-    "GTSDataset",
-    "STDNDataset",
-    "HGCNDataset",
-    "STAGGCNDataset",
-    'CONVGCNDataset',
-    "RESLSTMDataset",
-    "MultiSTGCnetDataset",
-    "CRANNDataset",
-    "CCRNNDataset",
-    "GeoSANDataset",
-    "DMVSTNetDataset",
     "MapMatchingDataset",
-    'ChebConvDataset',
-    "GSNetDataset",
-    "LINEDataset",
-    "CSTNDataset",
     "RoadNetWorkDataset"
 ]
