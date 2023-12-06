6614b5bfc296b500f70cee79caba460aeb27b684
Bai Rui <100181555+luiluizi@users.noreply.github.com>
2023-12-06 21:53:36 +0800

    ST-NORM update (#375)
    * ST-NORM update
    
    * solve pr
    
    * change loss func
    
    * fix
    
    * rename and change loss func

--- libcity/model/traffic_flow_prediction/__init__.py
+++ libcity/model/traffic_flow_prediction/__init__.py
@@ -20,6 +20,7 @@ from libcity.model.traffic_flow_prediction.CONVGCNCommon import CONVGCNCommon
 from libcity.model.traffic_flow_prediction.DSAN import DSAN
 from libcity.model.traffic_flow_prediction.MultiSTGCnetCommon import MultiSTGCnetCommon
 from libcity.model.traffic_flow_prediction.STGODE import STGODE
+from libcity.model.traffic_flow_prediction.STNorm import STNorm
 
 __all__ = [
     "AGCRN",
@@ -43,5 +44,6 @@ __all__ = [
     "CONVGCNCommon",
     "DSAN",
     "MultiSTGCnetCommon",
-    "STGODE"
+    "STGODE",
+    "STNorm"
 ]
