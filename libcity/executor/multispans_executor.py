16141c5286e394b83bcba0be8cb803f01b336264
wangyongyao <837826068@qq.com>
2024-05-09 16:46:21 +0800

    fix: executor

--- libcity/executor/multispans_executor.py
+++ libcity/executor/multispans_executor.py
@@ -4,7 +4,6 @@ from functools import partial
 
 import numpy as np
 import torch
-from ray import tune
 
 from libcity.executor.traffic_state_executor import TrafficStateExecutor
 from libcity.model import loss
@@ -13,8 +12,6 @@ from libcity.model import loss
 class MultiSPANSExecutor(TrafficStateExecutor):
     def __init__(self, config, model, data_feature):
         super().__init__(config, model, data_feature)
-        self.if_inv_loss = self.config.get("if_inv_loss", True)
-        self.if_inv_eval = self.config.get("if_inv_eval", True)
         self.pred_channel_idx = self.config.get("pred_channel_idx", None)
 
     def _build_train_loss(self):
@@ -35,10 +32,9 @@ class MultiSPANSExecutor(TrafficStateExecutor):
         def func(batch, channel_index):
             y_true = batch['y']
             y_predicted = self.model.predict(batch)
-            if self.if_inv_loss:
-                y_true = self._scaler.inverse_transform(y_true[..., :self.output_dim])
-                y_predicted = self._scaler.inverse_transform(y_predicted[..., :self.output_dim],
-                                                             channel_idx=channel_index)
+            y_true = self._scaler.inverse_transform(y_true[..., :self.output_dim])
+            y_predicted = self._scaler.inverse_transform(y_predicted[..., :self.output_dim],
+                                                         channel_idx=channel_index)
             if channel_index is not None:
                 y_true = y_true[..., channel_index]
             assert (y_true.shape[-1] == y_predicted.shape[-1]), 'Uncompatiable prediction & label channel!'
@@ -91,13 +87,9 @@ class MultiSPANSExecutor(TrafficStateExecutor):
                 batch.to_tensor(self.device)
                 output = self.model.predict(batch)
                 y_true = batch['y']
-                if self.if_inv_eval:
-                    y_true = self._scaler.inverse_transform(y_true[..., :self.output_dim])
-                    y_pred = self._scaler.inverse_transform(output[..., :self.output_dim],
-                                                            channel_idx=self.pred_channel_idx)
-                else:
-                    y_true = y_true[..., :self.output_dim]
-                    y_pred = output[..., :self.output_dim]
+                y_true = self._scaler.inverse_transform(y_true[..., :self.output_dim])
+                y_pred = self._scaler.inverse_transform(output[..., :self.output_dim],
+                                                        channel_idx=self.pred_channel_idx)
                 if self.pred_channel_idx is not None:
                     y_true = y_true[..., self.pred_channel_idx]
                 assert (
@@ -120,78 +112,6 @@ class MultiSPANSExecutor(TrafficStateExecutor):
             self.evaluator.collect({'y_true': torch.tensor(y_truths), 'y_pred': torch.tensor(y_preds)})
             test_result = self.evaluator.save_result(self.evaluate_res_dir)
             return test_result
-
-    def train(self, train_dataloader, eval_dataloader):
-        """
-        use data to train model with config
-
-        Args:
-            train_dataloader(torch.Dataloader): Dataloader
-            eval_dataloader(torch.Dataloader): Dataloader
-        """
-        self._logger.info('Start training ...')
-        min_val_loss = float('inf')
-        wait = 0
-        best_epoch = 0
-        train_time = []
-        eval_time = []
-        num_batches = len(train_dataloader)
-        self._logger.info("num_batches:{}".format(num_batches))
-
-        for epoch_idx in range(self._epoch_num, self.epochs):
-            start_time = time.time()
-            losses = self._train_epoch(train_dataloader, epoch_idx, self.loss_func)
-            t1 = time.time()
-            train_time.append(t1 - start_time)
-            self._writer.add_scalar('training loss', np.mean(losses), epoch_idx)
-            self._logger.info("epoch complete!")
-
-            t2 = time.time()
-            val_loss = self._valid_epoch(eval_dataloader, epoch_idx, self.loss_func)
-            end_time = time.time()
-            eval_time.append(end_time - t2)
-
-            if self.lr_scheduler is not None:
-                if self.lr_scheduler_type.lower() == 'reducelronplateau':
-                    self.lr_scheduler.step(val_loss)
-                else:
-                    self.lr_scheduler.step()
-
-            if (epoch_idx % self.log_every) == 0:
-                log_lr = self.optimizer.param_groups[0]['lr']
-                message = 'Epoch [{}/{}] train_loss: {:.4f}, val_loss: {:.4f}, lr: {:.6f}, {:.2f}s'. \
-                    format(epoch_idx, self.epochs, np.mean(losses), val_loss, log_lr, (end_time - start_time))
-                self._logger.info(message)
-
-            if self.hyper_tune:
-                # use ray tune to checkpoint
-                with tune.checkpoint_dir(step=epoch_idx) as checkpoint_dir:
-                    path = os.path.join(checkpoint_dir, "checkpoint")
-                    self.save_model(path)
-                # ray tune use loss to determine which params are best
-                tune.report(loss=val_loss)
-
-            if val_loss < min_val_loss:
-                wait = 0
-                if self.saved:
-                    model_file_name = self.save_model_with_epoch(epoch_idx)
-                    self._logger.info('Val loss decrease from {:.4f} to {:.4f}, '
-                                      'saving to {}'.format(min_val_loss, val_loss, model_file_name))
-                min_val_loss = val_loss
-                best_epoch = epoch_idx
-            else:
-                wait += 1
-                if wait == self.patience and self.use_early_stop:
-                    self._logger.warning('Early stopping at epoch: %d' % epoch_idx)
-                    break
-        if len(train_time) > 0:
-            self._logger.info('Trained totally {} epochs, average train time is {:.3f}s, '
-                              'average eval time is {:.3f}s'.
-                              format(len(train_time), sum(train_time) / len(train_time),
-                                     sum(eval_time) / len(eval_time)))
-        if self.load_best_epoch:
-            self.load_model_with_epoch(best_epoch)
-        return min_val_loss
 
     def _train_epoch(self, train_dataloader, epoch_idx, loss_func=None):
         """
@@ -244,38 +164,3 @@ class MultiSPANSExecutor(TrafficStateExecutor):
             mean_loss = np.mean(losses)
             self._writer.add_scalar('eval loss', mean_loss, epoch_idx)
             return mean_loss
-
-    def _test_epoch(self, test_dataloader, epoch_idx):
-        with torch.no_grad():
-            self.model.eval()
-            y_truths = []
-            y_preds = []
-            for batch in test_dataloader:
-                batch.to_tensor(self.device)
-                output = self.model.predict(batch)
-                y_true = batch['y']
-                if self.if_inv_eval:
-                    y_true = self._scaler.inverse_transform(y_true[..., :self.output_dim])
-                    y_pred = self._scaler.inverse_transform(output[..., :self.output_dim],
-                                                            channel_idx=self.pred_channel_idx)
-                else:
-                    y_true = y_true[..., :self.output_dim]
-                    y_pred = output[..., :self.output_dim]
-                if self.pred_channel_idx is not None:
-                    y_true = y_true[..., self.pred_channel_idx]
-                assert (
-                        y_true.shape[-1] == output.shape[-1]
-                ), 'Uncompatiable prediction & label channel!'
-                y_truths.append(y_true.cpu().numpy())
-                y_preds.append(y_pred.cpu().numpy())
-            y_preds = np.concatenate(y_preds, axis=0)
-            y_truths = np.concatenate(y_truths, axis=0)
-
-            self.evaluator.clear()
-            self.evaluator.collect({'y_true': torch.tensor(y_truths), 'y_pred': torch.tensor(y_preds)})
-            test_result = self.evaluator.evaluate()
-            self.evaluator.clear()
-            self._logger.info(f'Evaluate result of {epoch_idx} is ' +
-                              str({metric + '@' + str(12): test_result[metric + '@' + str(12)] for metric in
-                                   self.evaluator.metrics})
-                              )
