ca0d05faebcf77c695fc68bbb215213ea0a390c5
wangyongyao <837826068@qq.com>
2024-11-20 17:32:04 +0800

    feat: FreTS

---
+++ libcity/model/traffic_flow_prediction/FreTS.py
@@ -0,0 +1,125 @@
+from logging import getLogger
+
+import numpy as np
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+from libcity.model.abstract_traffic_state_model import AbstractTrafficStateModel
+from libcity.model import loss
+
+
+class FreTS(AbstractTrafficStateModel):
+    def __init__(self, config, data_feature):
+        super().__init__(config, data_feature)
+        self.device = config.get('device', torch.device('cpu'))
+        # section 1: data_feature
+        self._scaler = self.data_feature.get('scaler')
+
+        self.embed_size = config.get('embed_size', 128)
+        self.hidden_size = config.get('hidden_size', 256)
+        self.pre_length = config.get('output_window', 12)
+        self.num_nodes = self.data_feature.get('num_nodes', 1)
+        self.seq_length = config.get('input_window', 12)
+        self.feature_dim = self.data_feature.get('feature_dim', 1)
+        self.output_dim = self.data_feature.get('output_dim', 1)
+        self.channel_independence = config.get('channel_independence', True)
+        self.sparsity_threshold = config.get('sparsity_threshold', 0.01)
+        self.scale = config.get('scale', 0.02)
+        self.embeddings = nn.Parameter(torch.randn(self.feature_dim, self.embed_size))
+        self.r1 = nn.Parameter(self.scale * torch.randn(self.embed_size, self.embed_size))
+        self.i1 = nn.Parameter(self.scale * torch.randn(self.embed_size, self.embed_size))
+        self.rb1 = nn.Parameter(self.scale * torch.randn(self.embed_size))
+        self.ib1 = nn.Parameter(self.scale * torch.randn(self.embed_size))
+        self.r2 = nn.Parameter(self.scale * torch.randn(self.embed_size, self.embed_size))
+        self.i2 = nn.Parameter(self.scale * torch.randn(self.embed_size, self.embed_size))
+        self.rb2 = nn.Parameter(self.scale * torch.randn(self.embed_size))
+        self.ib2 = nn.Parameter(self.scale * torch.randn(self.embed_size))
+
+        self.fc = nn.Sequential(
+            nn.Linear(self.seq_length * self.embed_size, self.hidden_size),
+            nn.LeakyReLU(),
+            nn.Linear(self.hidden_size, self.pre_length * self.output_dim)
+        )
+
+    # dimension extension
+    def tokenEmb(self, x):
+        # [B, N, T, D]
+        x = x.permute(0, 2, 1, 3)
+        # N*T*1 x 1*D = N*T*D
+        y = self.embeddings
+        return x * y
+
+    # frequency temporal learner
+    def MLP_temporal(self, x, B, N, L):
+        # [B, N, T, D]
+        x = torch.fft.rfft(x, dim=2, norm='ortho') # FFT on L dimension
+        y = self.FreMLP(B, N, L, x, self.r2, self.i2, self.rb2, self.ib2)
+        x = torch.fft.irfft(y, n=self.seq_length, dim=2, norm="ortho")
+        return x
+
+    # frequency channel learner
+    def MLP_channel(self, x, B, N, L):
+        # [B, N, T, D]
+        x = x.permute(0, 2, 1, 3)
+        # [B, T, N, D]
+        x = torch.fft.rfft(x, dim=2, norm='ortho') # FFT on N dimension
+        y = self.FreMLP(B, L, N, x, self.r1, self.i1, self.rb1, self.ib1)
+        x = torch.fft.irfft(y, n=self.num_nodes, dim=2, norm="ortho")
+        # [B, T, N, D]
+        x = x.permute(0, 2, 1, 3)
+        # [B, N, T, D]
+        return x
+
+    # frequency-domain MLPs
+    # dimension: FFT along the dimension, r: the real part of weights, i: the imaginary part of weights
+    # rb: the real part of bias, ib: the imaginary part of bias
+    def FreMLP(self, B, nd, dimension, x, r, i, rb, ib):
+        o1_real = torch.zeros([B, nd, dimension // 2 + 1, self.embed_size],
+                              device=x.device)
+        o1_imag = torch.zeros([B, nd, dimension // 2 + 1, self.embed_size],
+                              device=x.device)
+
+        o1_real = F.relu(
+            torch.einsum('bijd,dd->bijd', x.real, r) - \
+            torch.einsum('bijd,dd->bijd', x.imag, i) + \
+            rb
+        )
+
+        o1_imag = F.relu(
+            torch.einsum('bijd,dd->bijd', x.imag, r) + \
+            torch.einsum('bijd,dd->bijd', x.real, i) + \
+            ib
+        )
+
+        y = torch.stack([o1_real, o1_imag], dim=-1)
+        y = F.softshrink(y, lambd=self.sparsity_threshold)
+        y = torch.view_as_complex(y)
+        return y
+
+    def predict(self, batch):
+        x = batch['X']
+        y = self.forward(x)
+        return y
+
+    def calculate_loss(self, batch):
+        y_true = batch['y']
+        y_predicted = self.predict(batch)
+        y_true = self._scaler.inverse_transform(y_true[..., :self.output_dim])
+        y_predicted = self._scaler.inverse_transform(y_predicted[..., :self.output_dim])
+        return loss.masked_mse_torch(y_predicted, y_true, 0.0)
+
+    def forward(self, x):
+        # x: [Batch, Input length, Channel]
+        B, T, N, D = x.shape
+        # embedding x: [B, N, T, D]
+        x = self.tokenEmb(x)
+        bias = x
+        # [B, N, T, D]
+        if self.channel_independence:
+            x = self.MLP_channel(x, B, N, T)
+        # [B, N, T, D]
+        x = self.MLP_temporal(x, B, N, T)
+        x = x + bias
+        x = self.fc(x.reshape(B, N, -1)).permute(0, 2, 1).reshape(B, T, N, -1)
+        return x
